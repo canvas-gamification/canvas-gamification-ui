d4c8ef59eeca350f4228f344ed926380a14b1609
Carson Ricca <carsonricca28@gmail.com>
2020-10-18 10:06:10 -0700

    Initial Contact Form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProblemSetComponent } from './problem-set/problem-set.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProblemSetComponent} from './problem-set/problem-set.component';
 import {MessagesComponent} from './messages/messages.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ContactService} from './services/contact.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemSetComponent,
     MessagesComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
