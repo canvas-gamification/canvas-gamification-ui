5e195988846c74af8c78d4ade158de2e24c516f5
opeyem1a <opeyadeyemi@gmail.com>
2020-10-21 01:58:09 -0600

    adding topics page w/ table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { TopicsComponent } from './components/topics/topics.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'topics',
+    component: TopicsComponent
   }
 ];
 
