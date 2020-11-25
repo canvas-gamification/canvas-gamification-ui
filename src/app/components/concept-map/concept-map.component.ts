5c71a2a0d7e6554ae338e99d2ea30d651892ef03
keyvankhademi <keyvankhademi@gmail.com>
2020-11-25 01:59:58 -0800

    Make clickable nodes

--- src/app/components/concept-map/concept-map.component.ts
+++ src/app/components/concept-map/concept-map.component.ts
@@ -1,10 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import * as jQuery from 'jquery';
-import * as _ from 'lodash';
-import * as $ from 'backbone';
-import dagre from 'dagre';
-import graphlib from 'graphlib';
-import * as joint from 'jointjs';
+import {ConceptMapGraph} from './concept-map-graph';
+
 
 @Component({
   selector: 'app-concept-map',
@@ -13,85 +9,53 @@ import * as joint from 'jointjs';
 })
 export class ConceptMapComponent implements OnInit {
 
-  makeElement(label) {
-    const maxLineLength = Math.max(...label.split('\n').map(x => x.length));
+  parentNode: string;
+  conceptMapGraph: ConceptMapGraph;
 
-    const letterSize = 8;
-    const width = 2 * (letterSize * (0.6 * maxLineLength + 1));
-    const height = 2 * ((label.split('\n').length + 1) * letterSize);
+  renderGraph() {
+    console.log(this.parentNode);
 
-    return new joint.shapes.basic.Rect({
-      id: label,
-      size: {width, height},
-      attrs: {
-        text: {text: label, 'font-size': letterSize, 'font-family': 'monospace'},
-        rect: {
-          width, height,
-          rx: 5, ry: 5,
-          stroke: '#555'
-        }
-      }
-    });
-  }
+    let adj: {};
 
-  makeLink(parentElementLabel, childElementLabel) {
-    return new joint.dia.Link({
-      source: {id: parentElementLabel},
-      target: {id: childElementLabel},
-      attrs: {'.marker-target': {d: 'M 4 0 L 0 2 L 4 4 z'}},
-      smooth: true
-    });
-  }
+    if (this.parentNode === 'Basics') {
+      adj = {
+        Variables: ['Statements', 'Data\nTypes', 'Constants'],
+        Statements: ['Arithmetic\nOperators'],
+        'Data\nTypes': ['Arithmetic\nOperators', 'Simple\nCalculation\nPrograms', 'Casting'],
+        Constants: ['Simple\nCalculation\nPrograms'],
+        'Arithmetic\nOperators': ['Simple\nCalculation\nPrograms'],
+        Casting: [],
+        'Simple\nCalculation\nPrograms': []
+      };
+    } else {
+      adj = {
+        Basics: ['Conditionals', 'Pre-Defined\nClasses'],
+        'Pre-Defined\nClasses': ['Object-Oriented\nPrograming'],
+        Conditionals: ['Pre-Defined\nClasses', 'Loops', 'Methods'],
+        Loops: ['Methods', 'Arrays'],
+        Methods: ['Object-Oriented\nPrograming'],
+        Arrays: ['Object-Oriented\nPrograming'],
+        'Object-Oriented\nPrograming': []
+      };
+    }
 
-  buildGraphFromAdjacencyList(adjacencyList) {
 
-    const elements = [];
-    const links = [];
-
-    _.each(adjacencyList, (edges, parentElementLabel) => {
-      elements.push(this.makeElement(parentElementLabel));
-
-      _.each(edges, (childElementLabel) => {
-        links.push(this.makeLink(parentElementLabel, childElementLabel));
-      });
-    });
-
-    return elements.concat(links);
+    this.conceptMapGraph.buildGraphFromAdjacencyList(adj);
   }
 
   constructor() {
   }
 
   ngOnInit() {
-    const adj = {
-      Basics: ['Conditionals', 'Pre-Defined\nClasses'],
-      'Pre-Defined\nClasses': ['Object-Oriented\nPrograming'],
-      Conditionals: ['Pre-Defined\nClasses', 'Loops', 'Methods'],
-      Loops: ['Methods', 'Arrays'],
-      Methods: ['Object-Oriented\nPrograming'],
-      Arrays: ['Object-Oriented\nPrograming'],
-      'Object-Oriented\nPrograming': []
-    };
-
-    const graph = new joint.dia.Graph();
-
-    const paper = new joint.dia.Paper({
-      el: jQuery('#paper'),
-      width: 2000,
-      height: 2000,
-      model: graph,
-      gridSize: 1
+    this.conceptMapGraph = new ConceptMapGraph((cellId) => {
+      this.parentNode = cellId;
+      this.renderGraph();
     });
+    this.renderGraph();
+  }
 
-    const cells = this.buildGraphFromAdjacencyList(adj);
-    graph.resetCells(cells);
-    joint.layout.DirectedGraph.layout(graph, {
-      dagre,
-      graphlib,
-      setLinkVertices: false,
-      nodeSep: 50,
-      edgeSep: 80,
-      rankDir: 'LR'
-    });
+  reset() {
+    this.parentNode = '';
+    this.renderGraph();
   }
 }
