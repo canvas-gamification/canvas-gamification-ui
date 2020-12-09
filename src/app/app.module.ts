06113772eceac7f2c857dc904be5d86acbab76a2
keyvankhademi <keyvankhademi@gmail.com>
2020-12-08 18:27:09 -0800

    Fix all the stupid issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-// For dummy topics API setup
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './fake-api-service/in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './components/messages/messages.component';
