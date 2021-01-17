06b4c98cb2f3226ea92c9c0cbb1330cf002043d9
keyvankhademi <keyvankhademi@gmail.com>
2021-01-17 12:38:02 -0800

    Project structure refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { ContactComponent } from './components/contact/contact.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ContactService } from './services/api/contact.service';
+import { ContactService } from './_services/api/contact.service';
 import { HeaderComponent } from './components/header/header.component';
 import { SampleQuestionsComponent } from './components/sample-questions/sample-questions.component';
 import { MatCardModule } from '@angular/material/card';
