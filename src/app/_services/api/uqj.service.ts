20932b952f3d6874038a71f6ccdc29c01afdfcf9
opeyem1a <opeyadeyemi@gmail.com>
2021-01-31 16:59:01 -0800

    renaming api endpoint

--- src/app/_services/api/uqj.service.ts
+++ src/app/_services/api/uqj.service.ts
@@ -10,7 +10,7 @@ import {UQJ} from '@app/_models/uqj';
 })
 export class UqjService {
   private userUqjUrl = new URL(
-    '/api/user-uqj',
+    '/api/uqj',
     environment.apiBaseUrl
   ).toString();
 
