457dc439e13256fbc12735fdf35f0802e7024b64
EliasPinno <pucas946@gmail.com>
2021-02-23 22:37:34 -0800

    Added basic html and hard coded some testing values in component

--- src/app/components/course/course-events-snippet/course-events-snippet.component.ts
+++ src/app/components/course/course-events-snippet/course-events-snippet.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseEventsSnippetComponent implements OnInit {
 
+  events = [
+    {
+      name: "ev1",
+      startDate: 1204,
+      endDate: 1819,
+      isExamAndOpen: true,
+      allowedToOpen: true,
+      eventBtnText: "buttonTxt1",
+      allowedToEdit: true
+    },
+    {
+      name: "ev2",
+      startDate: 2000,
+      endDate: 2020,
+      isExamAndOpen: true,
+      allowedToOpen: true,
+      eventBtnText: "buttonTxt1",
+      allowedToEdit: true
+    }
+  ]
+  request= {
+    user: {
+      isTeacher: true
+    }
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
