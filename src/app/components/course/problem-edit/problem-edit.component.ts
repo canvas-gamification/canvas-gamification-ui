dc7a6c472ff6e421aaa70c177b0310297a47eb29
Carson Ricca <carsonricca28@gmail.com>
2021-03-08 10:48:46 -0800

    Use Ng-Containers and Seperate FormGroups for Each Type of Question

--- src/app/components/course/problem-edit/problem-edit.component.ts
+++ src/app/components/course/problem-edit/problem-edit.component.ts
@@ -12,7 +12,9 @@ import {MessageService} from '@app/_services/message.service';
     styleUrls: ['./problem-edit.component.scss']
 })
 export class ProblemEditComponent implements OnInit {
-    FormData: FormGroup;
+    MCQFormData: FormGroup;
+    JavaFormData: FormGroup;
+    ParsonsFormData: FormGroup;
     private routeSub: Subscription;
     userId: number;
     questionType: string;
@@ -32,59 +34,72 @@ export class ProblemEditComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.FormData = this.formBuilder.group({
-            title: new FormControl(''),
-            difficulty: new FormControl(''),
-            category: new FormControl(''),
-            course_name: new FormControl(''),
-            event_name: new FormControl(''),
-            text: new FormControl(''),
-            answer: new FormControl(''),
-            junit_template: new FormControl(''),
-            visible_distractor_count: new FormControl('3')
-        });
         this.routeSub = this.route.params.subscribe(params => {
             this.userId = params.id;
         });
         this.questionService.getQuestion(this.userId).subscribe((detail: Question) => {
             this.QuestionDetails = detail;
-            this.FormData.controls.title.setValue(this.QuestionDetails.title);
-            this.FormData.controls.difficulty.setValue(this.QuestionDetails.difficulty);
-            this.FormData.controls.category.setValue(this.QuestionDetails.category_name);
-            this.FormData.controls.course_name.setValue(this.QuestionDetails.course_name);
-            this.FormData.controls.event_name.setValue(this.QuestionDetails.event_name);
-
             this.questionType = this.questionService.getQuestionType(this.QuestionDetails);
 
             if (this.questionType === 'multiple choice question') {
+                this.MCQFormData = this.formBuilder.group({
+                    title: new FormControl(''),
+                    difficulty: new FormControl(''),
+                    category: new FormControl(''),
+                    course_name: new FormControl(''),
+                    event_name: new FormControl(''),
+                    text: new FormControl(''),
+                    answer: new FormControl(''),
+                    visible_distractor_count: new FormControl('3')
+                });
+                this.MCQFormData.controls.title.setValue(this.QuestionDetails.title);
+                this.MCQFormData.controls.difficulty.setValue(this.QuestionDetails.difficulty);
+                this.MCQFormData.controls.category.setValue(this.QuestionDetails.category_name);
+                this.MCQFormData.controls.course_name.setValue(this.QuestionDetails.course_name);
+                this.MCQFormData.controls.event_name.setValue(this.QuestionDetails.event_name);
                 this.questionService.getMultipleChoiceQuestion(this.userId).subscribe((details: Question) => {
                     this.MultipleChoiceQuestionDetails = details;
                     this.variables = this.MultipleChoiceQuestionDetails.variables;
                     const outputArray = [];
                     // tslint:disable-next-line:forin
-                    for (const element in this.MultipleChoiceQuestionDetails.choices) {
+                    for (const choice in this.MultipleChoiceQuestionDetails.choices) {
                         outputArray.push({
-                            id: element,
-                            value: this.MultipleChoiceQuestionDetails.choices[element]
+                            id: choice,
+                            value: this.MultipleChoiceQuestionDetails.choices[choice]
                         });
                         this.choiceArray = outputArray;
                     }
                     this.correctAnswer = this.choiceArray[this.choiceArray
                         .findIndex(x => x.id === this.MultipleChoiceQuestionDetails.answer)];
 
-                    this.FormData.controls.text.setValue(this.MultipleChoiceQuestionDetails.text);
-                    this.FormData.controls.answer.setValue(this.correctAnswer.value);
+                    this.MCQFormData.controls.text.setValue(this.MultipleChoiceQuestionDetails.text);
+                    this.MCQFormData.controls.answer.setValue(this.correctAnswer.value);
 
                 });
             }
 
             if (this.questionType === 'java question') {
+                this.JavaFormData = this.formBuilder.group({
+                    title: new FormControl(''),
+                    difficulty: new FormControl(''),
+                    category: new FormControl(''),
+                    course_name: new FormControl(''),
+                    event_name: new FormControl(''),
+                    text: new FormControl(''),
+                    answer: new FormControl(''),
+                    junit_template: new FormControl(''),
+                });
                 this.questionService.getJavaQuestion(this.userId).subscribe((details: Question) => {
                     this.JavaQuestionDetails = details;
                     this.inputFileNames = this.JavaQuestionDetails.input_file_names;
                     this.variables = this.JavaQuestionDetails.variables;
-                    this.FormData.controls.text.setValue(this.JavaQuestionDetails.text);
-                    this.FormData.controls.junit_template.setValue(this.JavaQuestionDetails.junit_template);
+                    this.JavaFormData.controls.title.setValue(this.QuestionDetails.title);
+                    this.JavaFormData.controls.difficulty.setValue(this.QuestionDetails.difficulty);
+                    this.JavaFormData.controls.category.setValue(this.QuestionDetails.category_name);
+                    this.JavaFormData.controls.course_name.setValue(this.QuestionDetails.course_name);
+                    this.JavaFormData.controls.event_name.setValue(this.QuestionDetails.event_name);
+                    this.JavaFormData.controls.text.setValue(this.JavaQuestionDetails.text);
+                    this.JavaFormData.controls.junit_template.setValue(this.JavaQuestionDetails.junit_template);
                 });
             }
 
