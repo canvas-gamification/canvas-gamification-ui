0a8301be5a6133e088a2f5cc36132e7fb062dcdd
EliasPinno <pucas946@gmail.com>
2021-03-10 14:36:39 -0800

    Changed page to make add and subtract buttons limit appropriately

--- src/app/components/course/token-use-snippet/token-use-snippet.component.ts
+++ src/app/components/course/token-use-snippet/token-use-snippet.component.ts
@@ -3,6 +3,7 @@ import {faMinus, faPlus} from '@fortawesome/free-solid-svg-icons';
 import {CourseRegistration, TokenUseOption} from '@app/_models';
 import {TokenUseService} from '@app/_services/api/token-use.service';
 import {ActivatedRoute} from '@angular/router';
+import { TokenUse } from '@app/_models/token_use';
 
 @Component({
   selector: 'app-token-use-snippet',
@@ -11,11 +12,12 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class TokenUseSnippetComponent implements OnInit {
   @Input() courseReg: CourseRegistration;
-  @Input() tokenUseOptions: TokenUseOption[];
+
   tokenActions = {};
   courseId: number;
 
   invalid: boolean;
+  currentTotal: number;
 
   faMinus = faMinus;
   faPlus = faPlus;
@@ -30,23 +32,41 @@ export class TokenUseSnippetComponent implements OnInit {
       this.tokenActions[tokenUse.option.id] = tokenUse.num_used;
     });
     // Should always make invalid false unless some truly funky situations are happening
+    this.currentTotal = this.courseReg.available_tokens;
     this.canSave();
   }
 
   canSave() {
-    this.invalid = this.courseReg.available_tokens < 0;
+    this.invalid = this.currentTotal < 0;
+    return this.invalid;
   }
 
   formatFloat(value: number, fractionDigits: number): string {
     return value.toFixed(fractionDigits);
   }
 
-  incrementAction(optionId: number) {
-    ++this.tokenActions[optionId];
+  // Is this actually safe? I'm not 100% sure
+  incrementAction(token_use: TokenUse) {
+    console.log(this.tokenActions[token_use.option.id]+1)
+    this.changeTokenUse(token_use, this.tokenActions[token_use.option.id]+1)
   }
 
-  decrementAction(optionId: number) {
-    --this.tokenActions[optionId];
+  decrementAction(token_use: TokenUse) {
+    console.log(this.tokenActions[token_use.option.id]-1)
+    this.changeTokenUse(token_use, this.tokenActions[token_use.option.id]-1)
+  }
+
+  changeTokenUse(token_use: TokenUse, newVal: number){
+    let change = newVal - this.tokenActions[token_use.option.id];
+    if (newVal > token_use.option.maximum_number_of_use)
+      this.changeTokenUse(token_use, token_use.option.maximum_number_of_use);
+    else if (newVal < 0){
+      this.changeTokenUse(token_use,0)
+    }else{
+      this.tokenActions[token_use.option.id] = newVal
+      this.currentTotal -= change*token_use.option.tokens_required
+    }
+    this.canSave()
   }
 
   confirmChanges() {
