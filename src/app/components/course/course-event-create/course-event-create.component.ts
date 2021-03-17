dd55d24af302e11450f20f0eca4ef9c2a45261ec
opeyem1a <opeyadeyemi@gmail.com>
2021-03-17 14:36:24 -0700

    fixing tslint errors

--- src/app/components/course/course-event-create/course-event-create.component.ts
+++ src/app/components/course/course-event-create/course-event-create.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-course-event-create',
-  templateUrl: './course-event-create.component.html',
-  styleUrls: ['./course-event-create.component.scss']
+    selector: 'app-course-event-create',
+    templateUrl: './course-event-create.component.html',
+    styleUrls: ['./course-event-create.component.scss']
 })
 export class CourseEventCreateComponent implements OnInit {
 
 
-  courseId: number;
-  minDateISO: string;
+    courseId: number;
+    minDateISO: string;
 
-  constructor(private route: ActivatedRoute) { }
-  
-  // TODO: Reroute if you try and navigate to a page like this without clicking the button?
-  ngOnInit(): void {
-    // Convert to number
-    this.courseId = +this.route.snapshot.paramMap.get("courseId");
-    this.minDateISO = this.getMinDate();
-    console.log(this.minDateISO)
-  }
-  
-  getMinDate(): string{
-    let d = new Date()
-    return d.toISOString().split(".")[0];
-  }
+    constructor(private route: ActivatedRoute) {
+    }
+
+    // TODO: Reroute if you try and navigate to a page like this without clicking the button?
+    ngOnInit(): void {
+        // Convert to number
+        this.courseId = +this.route.snapshot.paramMap.get('courseId');
+        this.minDateISO = this.getMinDate();
+        console.log(this.minDateISO);
+    }
+
+    getMinDate(): string {
+        const d = new Date();
+        return d.toISOString().split('.')[0];
+    }
 
 }
