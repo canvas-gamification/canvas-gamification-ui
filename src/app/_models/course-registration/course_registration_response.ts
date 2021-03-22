a6fde4441047ccf552ec1b394964784534883287
opeyem1a <opeyadeyemi@gmail.com>
2021-03-22 03:35:00 -0700

    created models for the course registration request/response exchange

--- src/app/_models/course-registration/course_registration_response.ts
+++ src/app/_models/course-registration/course_registration_response.ts
@@ -1,6 +1,17 @@
 import {APIResponse} from '@app/_models';
 
 export interface CourseRegistrationResponse extends APIResponse {
+    name?: string;
     guessed_name?: string;
+    confirmed_name?: string;
+    student_number?: number;
+    code?: number;
     attempts_remaining?: number;
+}
+
+export interface CourseRegistrationRequest extends APIResponse {
+    name?: string;
+    confirmed_name?: string;
+    student_number?: number;
+    code?: number;
 }
