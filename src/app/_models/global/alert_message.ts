757802d33f77ae17eb4860d57cac8234766dabdd
opeyem1a <opeyadeyemi@gmail.com>
2021-03-22 03:01:13 -0700

    generalizing the message component to handle any kind of messages and still display them in chronological order

---
+++ src/app/_models/global/alert_message.ts
@@ -0,0 +1,10 @@
+export interface AlertMessage {
+    type: string;
+    message: string;
+}
+
+export const MESSAGE_TYPES = {
+    SUCCESS: 'success',
+    WARNING: 'warning',
+    DANGER: 'danger'
+};
