6023df344b53d223821bfce1996bc173473c6d6f
opeyem1a <opeyadeyemi@gmail.com>
2021-03-23 23:36:55 -0700

    tslint fixes

--- src/app/components/course/course-events-snippet/course-events-snippet.component.ts
+++ src/app/components/course/course-events-snippet/course-events-snippet.component.ts
@@ -1,43 +1,44 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CourseEvent, User} from '@app/_models';
-import {formatDate} from '@angular/common'
+import {formatDate} from '@angular/common';
 
 @Component({
-  selector: 'app-course-events-snippet',
-  templateUrl: './course-events-snippet.component.html',
-  styleUrls: ['./course-events-snippet.component.scss']
+    selector: 'app-course-events-snippet',
+    templateUrl: './course-events-snippet.component.html',
+    styleUrls: ['./course-events-snippet.component.scss']
 })
 export class CourseEventsSnippetComponent implements OnInit {
-  @Input() events: CourseEvent[];
-  @Input() courseId: number;
-  @Input() teacherForClass: boolean;
+    @Input() events: CourseEvent[];
+    @Input() courseId: number;
+    @Input() teacherForClass: boolean;
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
-  getEventButtonText(event: CourseEvent): string {
-    if(this.teacherForClass){
-      var strAppend = event.type.toLowerCase()
-      strAppend = strAppend.charAt(0).toUpperCase() + strAppend.substr(1,strAppend.length)
-      return 'Open ' + strAppend;
-    }else{
-      if (event.type == "ASSIGNMENT")
-        return "Complete Assignment"
-      else if(event.type == "PRACTICE")
-        return "Start Practice"
-      else if(event.type == "EXAM")
-        return "Take Exam"
-      else // Should never happen
-        return "Open"
+    constructor() {
     }
-    
-  }
 
-  isExamAndOpen(event: CourseEvent): boolean {
-    return event.is_open && event.is_exam;
-  }
+    ngOnInit(): void {
+    }
+
+    getEventButtonText(event: CourseEvent): string {
+        if (this.teacherForClass) {
+            let strAppend = event.type.toLowerCase();
+            strAppend = strAppend.charAt(0).toUpperCase() + strAppend.substr(1, strAppend.length);
+            return 'Open ' + strAppend;
+        } else {
+            if (event.type === 'ASSIGNMENT') {
+                return 'Complete Assignment';
+            } else if (event.type === 'PRACTICE') {
+                return 'Start Practice';
+            } else if (event.type === 'EXAM') {
+                return 'Take Exam';
+            } else // Should never happen
+            {
+                return 'Open';
+            }
+        }
+    }
+
+    isExamAndOpen(event: CourseEvent): boolean {
+        return event.is_open && event.is_exam;
+    }
 
 }
