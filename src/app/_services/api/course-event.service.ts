32ab068bc5f2921f090c2b0c153eb0b2ef059a49
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-03-23 19:36:49 -0700

    Added CourseEventService

---
+++ src/app/_services/api/course-event.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {CourseEvent} from '@app/_models';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {environment} from '@environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CourseEventService {
+    private courseEventUrl = new URL('/api/event/', environment.apiBaseUrl).toString();
+
+    constructor(
+        private http: HttpClient,
+    ) {
+    }
+
+    getCourseEvents(): Observable<CourseEvent[]> {
+        return this.http
+            .get<CourseEvent[]>(this.courseEventUrl)
+            .pipe(catchError(this.handleError<CourseEvent[]>('getCourseEvents', [])));
+    }
+
+    updateCourseEvent(courseEvent: CourseEvent): Observable<CourseEvent>{
+        return this.http.put<CourseEvent>(this.courseEventUrl + courseEvent.id + '/', courseEvent).pipe(
+            catchError(this.handleError<CourseEvent>('updateCourseEvent', courseEvent))
+        );
+    }
+
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+}
