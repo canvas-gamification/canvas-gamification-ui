69a9b1f46f8aed7cc47d0e40aeea8fad75a31498
EliasPinno <pucas946@gmail.com>
2021-03-23 21:05:15 -0700

    Using new interfaces to change component

--- src/app/components/course/course-event-create/course-event-create.component.ts
+++ src/app/components/course/course-event-create/course-event-create.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { CourseEventService } from '@app/_services/api/course-event.service';
 import { interval, Subscription } from 'rxjs';
+// TODO: Make changes to import properly from @app/_models for both
+// import { EventInfo, UpdateEvent} from '@app/_models';
+import { EventInfo } from '@app/_models/event_info';
+import { UpdateEvent } from '@app/_models/update_event';
 
 @Component({
     selector: 'app-course-event-create',
@@ -12,21 +17,26 @@ export class CourseEventCreateComponent implements OnInit {
     courseId: number;
     minDateISO: string;
     subscription: Subscription;
+    eventId: number;
 
     // Two way bound variables on inputs
     eventName: string;
     eventType: string;
-    countsForTokens: boolean;
+    countsForTokens: string;
     startTime: Date;
     endTime: Date;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private CourseEventService: CourseEventService) { }
   
   // TODO: Reroute if you try and navigate to a page like this without clicking the button?
   ngOnInit(): void {
     this.getMinDate()
     // Convert to number
     this.courseId = +this.route.snapshot.paramMap.get("courseId");
+    if (this.route.snapshot.paramMap.get("eventId")){
+      this.eventId = +this.route.snapshot.paramMap.get("eventId");
+      console.log("This is a previously existing event of id");
+    }
     const source = interval(10000);
     this.subscription = source.subscribe(() => this.getMinDate());
   }
@@ -36,6 +46,34 @@ export class CourseEventCreateComponent implements OnInit {
     let d = new Date();
     this.minDateISO = d.toISOString().split(".")[0];
 
+  }
+  //Hey
+
+  submitEvent(): void{
+    // Create the object here with the two way bound variables
+    let localCountsForTokens = false;
+    if(this.countsForTokens == "on"){
+      localCountsForTokens = true;
+    }
+    const ourEvent: EventInfo = {
+      name: this.eventName,
+      type: this.eventType,
+      count_for_tokens: localCountsForTokens,
+      start_date: new Date(this.startTime),
+      end_date: new Date(this.endTime),
+      course: this.courseId
+    }
+
+    if(this.eventId){ // If this is a previously existing event
+      const ourUpdateEvent: UpdateEvent = {
+        id: this.eventId,
+        event: ourEvent
+      };
+      this.CourseEventService.updateCourseEvent(ourUpdateEvent);
+    }
+    else{ // Creating a brand new event
+      this.CourseEventService.addCourseEvent(ourEvent);
+    }
   }
 
 }
