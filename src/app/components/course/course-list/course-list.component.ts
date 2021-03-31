30481ea29faaecc8335181ccc3d503b2129b4454
opeyem1a <opeyadeyemi@gmail.com>
2021-03-31 07:05:48 -0700

    massive amounts of tears traded to get the arrows to show up all the time in the course list mat-sort table + formatting

---
+++ src/app/components/course/course-list/course-list.component.ts
@@ -0,0 +1,35 @@
+import {Component, AfterViewInit, ViewChild} from '@angular/core';
+import {CourseService} from '@app/_services/api/course/course.service';
+import {ActivatedRoute} from '@angular/router';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.scss']
+})
+export class CourseListComponent implements AfterViewInit {
+    courseList: any;
+    displayedColumns: string[] = ['id', 'name', 'status', 'is_registered', 'actions'];
+
+    constructor(private route: ActivatedRoute, private courseService: CourseService) {
+        this.courseList = [];
+    }
+
+    @ViewChild(MatSort) sort: MatSort;
+
+    ngAfterViewInit() {
+        this.courseService.getCourses().subscribe((courses) => {
+            this.courseList = new MatTableDataSource(courses);
+            this.courseList.sort = this.sort;
+        });
+    }
+
+    hasViewPermission(courseId: number): boolean {
+        // TODO: return user.is_teacher or self.is_instructor(user) or self.is_registered(user)
+        return true;
+    }
+
+
+}
