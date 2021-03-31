0c5d95046ba650043a23362e0f26fd76f79d6f69
opeyem1a <opeyadeyemi@gmail.com>
2021-03-31 02:25:31 -0700

    + retrieve uqjs and event (kinda) object if this snippet is being used for an event-questions view

--- src/app/components/course/course-question-snippet/course-question-snippet.component.ts
+++ src/app/components/course/course-question-snippet/course-question-snippet.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Question, UQJ, User} from '@app/_models';
 import {AuthenticationService} from '@app/_services/api/authentication';
 import {ActivatedRoute} from '@angular/router';
+import {UqjService} from '@app/_services/api/uqj.service';
+import {forkJoin} from 'rxjs';
 
 @Component({
     selector: 'app-course-question-snippet',
@@ -12,14 +14,26 @@ export class CourseQuestionSnippetComponent implements OnInit {
     @Input() questions: Question[];
     @Input() uqjs: UQJ[]; // This will need to be commented out
     user: User;
+    event: any; // TODO: this should by of type Event
+    eventId: number;
 
-    constructor(private authenticationService: AuthenticationService, private route: ActivatedRoute) {
+    constructor(private authenticationService: AuthenticationService,
+                private route: ActivatedRoute,
+                private uqjService: UqjService, ) {
         this.authenticationService.currentUser.subscribe(user => this.user = user);
     }
 
     ngOnInit(): void {
         if (this.route.snapshot.paramMap.get('eventId')) { // We are opening an event
-            // uqjs = some api call
+            this.eventId = + this.route.snapshot.paramMap.get('eventId');
+            forkJoin({
+                // TODO: actually call events API (make the server)
+                event: this.uqjService.getUQJs({filters: {question__event: this.eventId, }}),
+                uqjs: this.uqjService.getUQJs({filters: {question__event: this.eventId, }}),
+            }).subscribe(result => {
+                this.event = result.event;
+                this.uqjs = result.uqjs.results;
+            });
         }
     }
 
