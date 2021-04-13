1d5f95d5461248dabd4ad0ab6a41abae342ab7e1
keyvankhademi <keyvankhademi@gmail.com>
2021-04-12 18:49:57 -0700

    Minor improvment to a code that is gonna be deleted lol

--- src/app/components/problems/problem-submission/problem-submission.component.ts
+++ src/app/components/problems/problem-submission/problem-submission.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-problem-submission',
-  templateUrl: './problem-submission.component.html',
-  styleUrls: ['./problem-submission.component.scss']
+    selector: 'app-problem-submission',
+    templateUrl: './problem-submission.component.html',
+    styleUrls: ['./problem-submission.component.scss']
 })
 export class ProblemSubmissionComponent implements OnInit {
-    private routeSub: Subscription;
     questionId: number;
 
-  constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-      this.routeSub = this.route.params.subscribe(params => {
-          this.questionId = params.id;
-      });
-  }
+    ngOnInit(): void {
+        this.questionId = this.route.snapshot.params.id;
+    }
 
 }
