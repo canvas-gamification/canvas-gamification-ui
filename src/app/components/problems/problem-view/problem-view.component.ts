176b1e1e38c81109d833b868c5a229bfa0258571
keyvankhademi <keyvankhademi@gmail.com>
2021-04-15 20:31:02 -0700

    add isAdmin to variables in question view

--- src/app/components/problems/problem-view/problem-view.component.ts
+++ src/app/components/problems/problem-view/problem-view.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {UQJ} from '@app/_models';
+import {UQJ, User} from '@app/_models';
 import {UqjService} from '@app/_services/api/uqj.service';
 import {QuestionSubmission} from '@app/_models/question_submission';
 import {SubmissionService} from '@app/_services/api/problem/submission.service';
+import {AuthenticationService} from '@app/_services/api/authentication';
 
 @Component({
     selector: 'app-problem-view',
@@ -14,11 +15,13 @@ export class ProblemViewComponent implements OnInit {
 
     constructor(private route: ActivatedRoute,
                 private uqjService: UqjService,
-                private submissionService: SubmissionService) {
+                private submissionService: SubmissionService,
+                private authenticationService: AuthenticationService) {
     }
 
     UQJDetails: UQJ;
     previousSubmissions: QuestionSubmission[];
+    user: User;
 
     ngOnInit(): void {
         const questionId = this.route.snapshot.params.id;
@@ -28,6 +31,10 @@ export class ProblemViewComponent implements OnInit {
 
         this.submissionService.getPreviousSubmissions(questionId).subscribe(submissions => {
             this.previousSubmissions = submissions;
+        });
+
+        this.authenticationService.currentUser.subscribe(user => {
+            this.user = user;
         });
     }
 }
