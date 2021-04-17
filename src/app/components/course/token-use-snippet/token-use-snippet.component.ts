86f8f7a5a7095d2d6a1b0a121e2b9da2fd0a128c
EliasPinno <pucas946@gmail.com>
2021-04-17 03:22:05 -0600

    Decimals UI confirmation (#94)
    * Fixed token-use confirm button to not send requests when html is manually changed
    
    * Added token use option change save message.
    
    * Added number pipes in places where it may be required
    
    Pipes ensure 1 decimal place max.
    
    * Corrected a spelling error
    
    * PR fixes
    
    * PR fixes
    
    * Fix conflicts
    
    * Refactor toke use
    
    Co-authored-by: opeyem1a <opeyadeyemi@gmail.com>
    Co-authored-by: keyvankhademi <keyvankhademi@gmail.com>

--- src/app/components/course/token-use-snippet/token-use-snippet.component.ts
+++ src/app/components/course/token-use-snippet/token-use-snippet.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {faMinus, faPlus} from '@fortawesome/free-solid-svg-icons';
-import {CourseRegistration, User} from '@app/_models';
+import {CourseRegistration, MESSAGE_TYPES, User} from '@app/_models';
 import {TokenUseService} from '@app/_services/api/token-use.service';
 import {ActivatedRoute} from '@angular/router';
 import {TokenUse} from '@app/_models/token_use';
 import {AuthenticationService} from '@app/_services/api/authentication';
+import {MessageService} from '@app/_services/message.service';
 
 @Component({
     selector: 'app-token-use-snippet',
@@ -13,80 +14,55 @@ import {AuthenticationService} from '@app/_services/api/authentication';
 })
 export class TokenUseSnippetComponent implements OnInit {
     @Input() courseReg: CourseRegistration;
+
+    tokenUses: {
+        [index: number]: TokenUse;
+    } = {};
+
     user: User;
 
-    currentTokenActions = {};
-    prevTokenActions = {};
-    courseId: number;
-
     invalid: boolean;
-    currentTotal: number;
+    remainingTokens: number;
 
     faMinus = faMinus;
     faPlus = faPlus;
 
     constructor(private tokenUseService: TokenUseService,
                 private route: ActivatedRoute,
-                private authenticationService: AuthenticationService) {
-        this.courseId = this.route.snapshot.params.courseId;
+                private authenticationService: AuthenticationService,
+                private messageService: MessageService) {
         this.authenticationService.currentUser.subscribe(user => this.user = user);
     }
 
     ngOnInit(): void {
         this.courseReg.token_uses.forEach(tokenUse => {
-            this.currentTokenActions[tokenUse.option.id] = tokenUse.num_used;
-            // Have to keep track of previous to detect changes when user types, due to double binding
-            this.prevTokenActions[tokenUse.option.id] = tokenUse.num_used;
+            this.tokenUses[tokenUse.option.id] = tokenUse;
         });
-        // Should always make invalid false unless some truly funky situations are happening
-        this.currentTotal = this.courseReg.available_tokens;
-        this.invalid = this.currentTotal < 0;
+        this.calculateCurrentTotal();
     }
 
-    formatFloat(value: number, fractionDigits: number): string {
-        return value.toFixed(fractionDigits);
+    useToken(tokenUse: TokenUse, val: number) {
+        tokenUse.num_used += val;
+        this.calculateCurrentTotal();
     }
 
-    incrementAction(tokenUse: TokenUse) {
-        this.changeTokenUse(tokenUse, this.currentTokenActions[tokenUse.option.id] + 1);
-    }
-
-    decrementAction(tokenUse: TokenUse) {
-        this.changeTokenUse(tokenUse, this.currentTokenActions[tokenUse.option.id] - 1);
-    }
-
-    changeTokenUse(tokenUse: TokenUse, newVal: number) {
-        // Handles user typing a change... basically change the linked value to what it was earlier, and redo the change properly
-        // Since input is double bound, if user types a change and this event is called, you can't get the delta with the previous value
-        if (this.currentTokenActions[tokenUse.option.id] !== this.prevTokenActions[tokenUse.option.id]) {
-            const temp = this.currentTokenActions[tokenUse.option.id];
-            this.currentTokenActions[tokenUse.option.id] = this.prevTokenActions[tokenUse.option.id];
-            this.changeTokenUse(tokenUse, temp);
+    calculateCurrentTotal() {
+        this.remainingTokens = this.courseReg.total_tokens_received;
+        for (const optionId in this.tokenUses) {
+            this.remainingTokens -= this.tokenUses[optionId].num_used * this.tokenUses[optionId].option.tokens_required;
         }
-        const change = newVal - this.currentTokenActions[tokenUse.option.id];
-        if (newVal > tokenUse.option.maximum_number_of_use) {
-            this.changeTokenUse(tokenUse, tokenUse.option.maximum_number_of_use);
-            return;
-        } else if (newVal < 0) {
-            this.changeTokenUse(tokenUse, 0);
-            return;
-        } else {
-            this.currentTokenActions[tokenUse.option.id] = newVal;
-            this.prevTokenActions[tokenUse.option.id] = newVal;
-            this.currentTotal -= change * tokenUse.option.tokens_required;
-        }
-        this.invalid = this.currentTotal < 0;
-    }
-
-    typingChanges(tokenUse: TokenUse, newVal: number) {
-        this.changeTokenUse(tokenUse, this.currentTokenActions[tokenUse.option.id]);
+        this.invalid = this.remainingTokens < 0;
     }
 
     confirmChanges() {
-        this.tokenUseService.useTokens(this.currentTokenActions, this.courseId).subscribe(apiResponse => {
-            if (!apiResponse.success) {
-                window.location.reload();
-            }
+        const courseId = this.route.snapshot.params.courseId;
+        const data = {};
+        for (const optionId in this.tokenUses) {
+            data[optionId] = this.tokenUses[optionId].num_used;
+        }
+
+        this.tokenUseService.useTokens(data, courseId).subscribe(() => {
+            this.messageService.add(MESSAGE_TYPES.SUCCESS, 'Token uses saved!.');
         });
     }
 }
