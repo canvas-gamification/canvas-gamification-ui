aa994ec3a1b6dcad8a5ec196b466f20dc5318bb8
opeyem1a <opeyadeyemi@gmail.com>
2021-04-16 19:54:11 -0700

    refactored classes/ids in FAQ component + small UI improvements + confirmed proper data flow

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FaqService } from '@app/_services/api/faq.service';
-import { Faq } from '@app/_models/faq';
+import {Component, OnInit} from '@angular/core';
+import {FaqService} from '@app/_services/api/faq.service';
+import {Faq} from '@app/_models/faq';
 
 @Component({
-  selector: 'app-faq',
-  templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+    selector: 'app-faq',
+    templateUrl: './faq.component.html',
+    styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
 
-  faqs: Faq[];
+    faqs: Faq[];
 
-  constructor(public faqService: FaqService) {}
+    constructor(public faqService: FaqService) {
+    }
 
-  ngOnInit(): void {
-    this.faqService
-    .getFaqs()
-    .subscribe((faqs) => {
-      this.faqs = faqs;
-    });
-  }
+    ngOnInit(): void {
+        this.faqService
+            .getFaqs()
+            .subscribe((faqs) => {
+                this.faqs = faqs;
+            });
+    }
 
 }
