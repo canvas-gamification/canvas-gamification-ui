0dc230ccde599a0fd11dfa63996dbc0edce2be9b
EliasPinno <pucas946@gmail.com>
2021-04-28 18:05:18 -0700

    Copy pasting standard service error logging (should really inherit from something)

--- src/app/_services/api/admin/category-stats.service.ts
+++ src/app/_services/api/admin/category-stats.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '@environments/environment.prod';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CategoryStatsService {
+    private categoryStatsUrl = new URL('/api/admin/category-stats', environment.apiBaseUrl);
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
 }
