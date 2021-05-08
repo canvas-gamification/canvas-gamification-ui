2e585fc82a21792a6f5a777e1834887384569842
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-08 17:54:39 -0400

    Fixes ESLint for Accounts (#124)
    * Fixes ESLint for Accounts
    
    - Fixes all ESLint issues for the accounts component.
    
    * Make Fixed Based on PR Comments

--- src/app/components/accounts/reset-password/reset-password.component.spec.ts
+++ src/app/components/accounts/reset-password/reset-password.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ResetPasswordComponent } from './reset-password.component';
+import {ResetPasswordComponent} from './reset-password.component';
 
 describe('ResetPasswordComponent', () => {
-  let component: ResetPasswordComponent;
-  let fixture: ComponentFixture<ResetPasswordComponent>;
+    let component: ResetPasswordComponent;
+    let fixture: ComponentFixture<ResetPasswordComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ResetPasswordComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResetPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ResetPasswordComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/accounts/reset-password/reset-password.component.ts
+++ src/app/components/accounts/reset-password/reset-password.component.ts
@@ -7,26 +7,26 @@ import {MESSAGE_TYPES} from '@app/_models';
 @Component({
     selector: 'app-reset-password',
     templateUrl: './reset-password.component.html',
-    styleUrls: ['./reset-password.component.scss']
+    styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
-    FormData: FormGroup;
+    formData: FormGroup;
 
     constructor(private builder: FormBuilder, private password: ResetPasswordService, private messageService: MessageService) {
     }
 
     ngOnInit(): void {
-        this.FormData = this.builder.group({
+        this.formData = this.builder.group({
             old_password: new FormControl('', [Validators.required]),
             password: new FormControl('', [Validators.required]),
             password2: new FormControl('', [Validators.required])
         });
     }
 
-    onSubmit(FormData) {
-        this.password.PutPasswordReset(FormData)
-            .subscribe(response => {
-                this.FormData.reset();
+    onSubmit(formData: FormGroup): void {
+        this.password.PutPasswordReset(formData)
+            .subscribe(() => {
+                this.formData.reset();
                 this.messageService.add(MESSAGE_TYPES.SUCCESS, 'Your password has been updated successfully!');
             }, error => {
                 console.warn(error.responseText);
