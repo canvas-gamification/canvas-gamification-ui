d67c63351aac92d615dc2b9a978ff2a99b52b712
keyvankhademi <keyvankhademi@gmail.com>
2021-05-08 23:06:55 -0700

    Fix deploy

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+        browsers: ['ChromeHeadless', 'ChromeHeadlessCI'],
         singleRun: false,
         restartOnFileChange: true,
         customLaunchers: {
