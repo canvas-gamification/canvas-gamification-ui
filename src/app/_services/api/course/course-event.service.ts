804680a68da77bcb4e58509a9def45615aa5501a
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-05-11 18:34:07 -0700

    Linted all of course services and user stats. Removed course registration service since it was redundant.

--- src/app/_services/api/course/course-event.service.ts
+++ src/app/_services/api/course/course-event.service.ts
@@ -54,11 +54,10 @@ export class CourseEventService {
      * @param operation - name of the operation that failed
      * @param result - optional value to return as the observable result
      */
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
+    private handleError<T>(operation?, result?: T) {
+        return (error: string): Observable<T> => {
             // TODO: send the error to remote logging infrastructure
             console.error(error); // log to console instead
-
             // Let the app keep running by returning an empty result.
             return of(result as T);
         };
