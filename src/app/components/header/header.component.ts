de58692ab35eee5f9ad87dbaa9e5b00af46032b2
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-05-12 21:09:47 -0400

    Fix Linting Issues

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,33 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {User} from '@app/_models';
 import {AuthenticationService} from '@app/_services/api/authentication';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  user: User;
-  path: string;
-  logoPath = 'assets/global/logo.jpg';
+export class HeaderComponent {
+    user: User;
+    path: string;
+    logoPath = 'assets/global/logo.jpg';
 
-  constructor(private authenticationService: AuthenticationService, private router: Router, private route: ActivatedRoute) {
-    this.authenticationService.currentUser.subscribe(user => {
-        this.user = user;
-    });
-  }
+    constructor(private authenticationService: AuthenticationService, private router: Router, private route: ActivatedRoute) {
+        this.authenticationService.currentUser.subscribe(user => {
+            this.user = user;
+        });
+    }
 
-  ngOnInit(): void {
-  }
+    logout(): void {
+        this.authenticationService.logout();
+        this.router.navigate(['../../'], {relativeTo: this.route}).then();
+    }
 
-  logout(): void {
-    this.authenticationService.logout();
-    this.router.navigate(['../../'], {relativeTo: this.route});
-  }
-
-  isActive(navLink) {
-    return window.location.pathname.slice(1) === navLink;
-  }
+    isActive(navLink): boolean {
+        return window.location.pathname.slice(1) === navLink;
+    }
 }
