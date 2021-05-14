a632a4210a083c262116a22e6474d2a9a336c570
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-05-14 15:12:19 -0700

    linted homepage component and some of the course component

--- src/app/components/homepage/course-dashboard/course-dashboard.component.ts
+++ src/app/components/homepage/course-dashboard/course-dashboard.component.ts
@@ -3,9 +3,9 @@ import {CourseService} from '@app/_services/api/course/course.service';
 import {Course, STATUS} from '@app/_models';
 
 @Component({
-  selector: 'app-course-dashboard',
-  templateUrl: './course-dashboard.component.html',
-  styleUrls: ['./course-dashboard.component.scss']
+    selector: 'app-course-dashboard',
+    templateUrl: './course-dashboard.component.html',
+    styleUrls: ['./course-dashboard.component.scss']
 })
 export class CourseDashboardComponent implements OnInit {
   activeCourses: Course[];
@@ -14,12 +14,12 @@ export class CourseDashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.courseService
-      .getCourses(true, {ordering: {name: true}})
-      ?.subscribe((courses) => {
-        this.activeCourses = courses.filter(course => {
-          return course.status === STATUS.active;
-        });
-      });
+      this.courseService
+          .getCourses(true, {ordering: {name: true}})
+          ?.subscribe((courses) => {
+              this.activeCourses = courses.filter(course => {
+                  return course.status === STATUS.active;
+              });
+          });
   }
 }
--- src/app/components/homepage/recent-viewed-questions/recent-viewed-questions.component.ts
+++ src/app/components/homepage/recent-viewed-questions/recent-viewed-questions.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {UQJ} from '@app/_models/uqj';
 import {UqjService} from '@app/_services/api/uqj.service';
+import {Question} from "@app/_models";
 
 @Component({
-  selector: 'app-recent-viewed-questions',
-  templateUrl: './recent-viewed-questions.component.html',
-  styleUrls: ['./recent-viewed-questions.component.scss']
+    selector: 'app-recent-viewed-questions',
+    templateUrl: './recent-viewed-questions.component.html',
+    styleUrls: ['./recent-viewed-questions.component.scss']
 })
 export class RecentViewedQuestionsComponent implements OnInit {
   uqjs: UQJ[];
@@ -16,15 +17,15 @@ export class RecentViewedQuestionsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.uqjService
-      .getUQJs({recent: true, page_size: 5})
-      ?.subscribe((paginatedUqjs) => {
-        this.uqjs = paginatedUqjs.results;
-      });
+      this.uqjService
+          .getUQJs({recent: true, page_size: 5})
+          ?.subscribe((paginatedUqjs) => {
+              this.uqjs = paginatedUqjs.results;
+          });
   }
 
-  formatQuestion(question): string {
-    return `${question.title} (${question.id})`;
+  formatQuestion(question: Question): string {
+      return `${question.title} (${question.id})`;
   }
 
 }
