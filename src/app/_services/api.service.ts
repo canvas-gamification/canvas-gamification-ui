627d4c9c3f7ea6bd56ac0494f88ef6e907c91f73
Keyvan <keyvankhademi@gmail.com>
2021-05-26 21:08:27 -0700

    Modify api service to work with master

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -1,16 +1,17 @@
 import {Injectable} from '@angular/core';
 import {environment} from "@environments/environment";
 import {Observable, of} from "rxjs";
-import {MESSAGE_TYPES} from "@app/_models";
-import {MessageService} from "@app/_services/message.service";
 import {HttpParams} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
-    constructor(private messageService: MessageService, private router: Router) {
+    constructor(
+        private router: Router,
+        private toastr: ToastrService) {
     }
 
     addParams(params?: Record<string, string>): { params: HttpParams } {
@@ -39,7 +40,7 @@ export class ApiService {
             else if (error.localeCompare('Forbidden', undefined, {sensitivity: 'base'}) === 0)
                 this.router.navigate(['/403']).then();
 
-            this.messageService.add(MESSAGE_TYPES.DANGER, message);
+            this.toastr.error(message);
             return of(result as T);
         };
     }
