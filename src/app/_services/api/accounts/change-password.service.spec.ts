758a9e4bc59ea1d870f76cd45d162fa9d2b444e5
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-06-01 15:56:07 -0700

    Restored old reset password component and links, but renamed it to change password, protected by authGuard

---
+++ src/app/_services/api/accounts/change-password.service.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TestModule} from '@test/test.module';
+import {ChangePasswordComponent} from "@app/components/accounts/change-password/change-password.component";
+
+describe('ChangePasswordComponent', () => {
+    let component: ChangePasswordComponent;
+    let fixture: ComponentFixture<ChangePasswordComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TestModule]
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ChangePasswordComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import {ProblemSetComponent} from '@app/components/problems/problem-set/problem-
 import {RegisterComponent} from './components/accounts/register/register.component';
 import {ProfileDetailsComponent} from './components/accounts/profile-details/profile-details.component';
 import {ResetPasswordComponent} from './components/accounts/reset-password/reset-password.component';
+import {ChangePasswordComponent} from '@app/components/accounts/change-password/change-password.component';
 import {ConsentFormComponent} from '@app/components/accounts/consent-form/consent-form.component';
 import {FaqComponent} from './components/faq/faq.component';
 import {HomepageComponent} from './components/homepage/homepage.component';
@@ -26,8 +27,8 @@ import {ProblemEditComponent} from '@app/components/problems/problem-edit/proble
 import {ProblemCreateComponent} from '@app/components/problems/problem-create/problem-create.component';
 import {ActivationEmailComponent} from '@app/components/accounts/activation-email/activation-email.component';
 import {SubmissionViewComponent} from '@app/components/problems/submission-view/submission-view.component';
-import {NotFoundComponent} from "@app/components/general/not-found/not-found.component";
-import {ForbiddenComponent} from "@app/components/general/forbidden/forbidden.component";
+import {NotFoundComponent} from '@app/components/general/not-found/not-found.component';
+import {ForbiddenComponent} from '@app/components/general/forbidden/forbidden.component';
 
 
 const routes: Routes = [
@@ -80,6 +81,11 @@ const routes: Routes = [
     {
         path: 'accounts/profile',
         component: ProfileDetailsComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'accounts/change-password',
+        component: ChangePasswordComponent,
         canActivate: [AuthGuard]
     },
     {
