bf66d38546e3618a480c60d975b15e0417828f4c
nakheem <rayman55@live.ca>
2021-06-08 09:48:30 -0700

    dont pull pls

---
+++ src/app/components/leader-board/leader-board-list/course-list.component.spec.ts
@@ -0,0 +1,25 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {CourseListComponent} from './course-list.component';
+import {TestModule} from '../../../../test/test.module';
+
+describe('CourseListComponent', () => {
+    let component: CourseListComponent;
+    let fixture: ComponentFixture<CourseListComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TestModule]
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CourseListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
