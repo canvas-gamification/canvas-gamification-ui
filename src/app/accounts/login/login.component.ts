10c4b71f409bea17d71fc34f8bf46ffec786355f
Keyvan <keyvankhademi@gmail.com>
2021-06-08 22:10:57 -0700

    Login page errors

--- src/app/accounts/login/login.component.ts
+++ src/app/accounts/login/login.component.ts
@@ -1,10 +1,10 @@
 ﻿import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
 import {first} from 'rxjs/operators';
 
 import {AuthenticationService} from '@app/_services/api/authentication';
-import {ConsentService} from '@app/accounts/_services/consent.service';
+import {LoginForm} from "@app/accounts/_forms/login.form";
 
 @Component({
     selector: 'app-login',
@@ -12,7 +12,7 @@ import {ConsentService} from '@app/accounts/_services/consent.service';
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
+    formGroup: FormGroup;
     loading = false;
     submitted = false;
     error = '';
@@ -23,48 +23,36 @@ export class LoginComponent implements OnInit {
         private route: ActivatedRoute,
         private router: Router,
         private authenticationService: AuthenticationService,
-        private consentService: ConsentService
     ) {
-        // redirect to home if already logged in
         if (this.authenticationService.currentUserValue) {
             this.router.navigate(['/homepage']).then();
         }
     }
 
     ngOnInit(): void {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+        this.formGroup = LoginForm.createForm();
     }
 
-    // convenience getter for easy access to form fields
-    get f(): { [p: string]: AbstractControl } {
-        return this.loginForm.controls;
+    get form(): { [p: string]: AbstractControl } {
+        return this.formGroup.controls;
     }
 
     onSubmit(): void {
         this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
         this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
+
+        this.authenticationService.login(this.form.username.value, this.form.password.value)
             .pipe(first())
             .subscribe({
-                next: () => {
-                    this.consentService.getConsent().subscribe((consents) => {
-                        if (consents.length === 0) {
-                            this.router.navigate(['/accounts/consent-form']).then();
-                        } else {
-                            // get return url from route parameters or default to '/'
-                            const returnUrl = this.route.snapshot.queryParams.returnUrl || '/homepage';
-                            this.router.navigate([returnUrl]).then();
-                        }
-                    });
+                next: (user) => {
+                    if (!user.has_consent) {
+                        this.router.navigate(['/accounts/consent-form']).then();
+                    } else {
+                        // get return url from route parameters or default to '/'
+                        const returnUrl = this.route.snapshot.queryParams.returnUrl || '/homepage';
+                        this.router.navigate([returnUrl]).then();
+                    }
+
                 },
                 error: error => {
                     this.error = error;
