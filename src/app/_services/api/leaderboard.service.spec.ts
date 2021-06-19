74c9e207635fc1cf75ab01802a1df10f6779537e
Jarod <jarodalgra@gmail.com>
2021-06-18 18:19:48 -0700

    linted

--- src/app/_services/api/leaderboard.service.spec.ts
+++ src/app/_services/api/leaderboard.service.spec.ts
@@ -1,19 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import {
+    TestBed
+} from '@angular/core/testing';
 
-import { LeaderboardService } from './leaderboard.service';
-import {TestModule} from '@test/test.module';
+import {
+    LeaderboardService
+} from './leaderboard.service';
+import {
+    TestModule
+} from '@test/test.module';
 
 describe('LeaderboardService', () => {
-  let service: LeaderboardService;
+    let service: LeaderboardService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [TestModule]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [TestModule]
+        });
+        service = TestBed.inject(LeaderboardService);
     });
-    service = TestBed.inject(LeaderboardService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
