d98538d937fd1ed65d0990da83bc4dfcafb5a431
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-06-21 21:02:08 -0400

    Schema Service 100% Coverage

--- src/app/problems/_services/schema.service.spec.ts
+++ src/app/problems/_services/schema.service.spec.ts
@@ -2,19 +2,56 @@ import {TestBed} from '@angular/core/testing';
 
 import {SchemaService} from './schema.service';
 import {TestModule} from '@test/test.module';
+import {ApiService} from "@app/_services/api.service";
+import {HttpTestingController} from "@angular/common/http/testing";
 
 describe('SchemaService', () => {
-    let service: SchemaService;
+    const mockSchemas: any = [
+        {
+            title: 'TestSchema',
+            type: 'array',
+            format: 'table',
+            items: {
+                type: 'string',
+                title: 'Test'
+            }
+        },
+        {
+            title: 'TestSchema2',
+            type: 'array',
+            format: 'table',
+        }
+    ];
 
+    let schemaService: SchemaService;
+    let apiService: ApiService;
+    let httpMock: HttpTestingController;
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [TestModule],
-            providers: [SchemaService]
+            providers: [SchemaService, ApiService]
         });
-        service = TestBed.inject(SchemaService);
+        schemaService = TestBed.inject(SchemaService);
+        apiService = TestBed.inject(ApiService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(schemaService).toBeTruthy();
+    });
+
+    it('getSchema returns schema', () => {
+        schemaService.getSchema('TestSchema').subscribe((schema) => {
+            expect(schema.title).toEqual('TestSchema');
+            expect(schema.type).toEqual('array');
+            expect(schema.format).toEqual('table');
+        });
+        const request = httpMock.expectOne(apiService.getURL('schema', 'TestSchema'));
+        expect(request.request.method).toBe('GET');
+        request.flush(mockSchemas.find(schema => schema.title === 'TestSchema'));
     });
 });
