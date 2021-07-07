7bde4e68a99015c803e6f13b7f315c2958024a64
JarodAlgra <jarodalgra@gmail.com>
2021-07-06 21:39:53 -0700

    Using Material Table
    using matTable and course service to populate leaderboard

--- src/app/course/leader-board/leader-board.component.ts
+++ src/app/course/leader-board/leader-board.component.ts
@@ -1,75 +1,97 @@
-import { Component, OnInit } from '@angular/core';
-// import {MatTableDataSource} from '@angular/material/table';
-
+import {
+    Component,
+    OnInit,
+    Input,
+    ViewChild,
+    AfterViewInit
+} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 import {LeaderBoardService} from '@app/_services/api/leaderboard.service';
-//import {TestModel} from '@app/_models/test_model';
-import { LeaderBoardStudents } from '@app/_models/leader_board';
+
+import {LeaderBoardStudents} from '@app/_models/leader_board';
 
 @Component({
     selector: 'app-leader-board',
     templateUrl: './leader-board.component.html',
     styleUrls: ['./leader-board.component.scss']
 })
-export class LeaderBoardComponent implements OnInit {
+export class LeaderBoardComponent implements OnInit, AfterViewInit {
 
-  // leaderBoard: LeaderBoardStudents[];
-  value = "Hello World!";
-  users: LeaderBoardStudents[];
-  topThree : LeaderBoardStudents[] = [];
-  constructor(private leaderboardService: LeaderBoardService) { }
+    // leaderBoard: LeaderBoardStudents[];
+    value = "Hello World!";
+    users: LeaderBoardStudents[];
+    topThree: LeaderBoardStudents[] = [];
 
-  ngOnInit(): void {
-      this.leaderboardService
-          .getLeaderBoard()
-          .subscribe((users) => {
-              console.log(users);
-              this.users = users.sort((a, b) => {
-                  if(a.token_value < b.token_value){
-                      return 1;
-                  }
-                  if(a.token_value > b.token_value){
-                      return -1;
-                  }
-                  return 0;
-              });
+    @Input() leaderBoard: [{
+        name: string,
+        token: number,
+    }];
 
-              for(let i = 0; i < 3; i++) {
-                  this.topThree.push(this.users.shift());  
-              }
-          });
+    displayedColumns: string[] = ['rank', 'name', 'token'];
+    leaderBoardData : MatTableDataSource<{
+        name: string,
+        token: number,
+    }>;
+    @ViewChild(MatSort) matSort: MatSort;
+    
 
-  }
+    constructor(private leaderboardService: LeaderBoardService) {}
 
-  turnToGif(e: Event) : void {
-      const parent = e.target as HTMLElement;
-      if(parent.children[2].children[0]){
-          const img = parent.children[2].children[0].children[0] as HTMLImageElement;
-          const fire: string = window.location.origin + '/assets/gif/fire-still.png';
-          const snow: string = window.location.origin + '/assets/gif/snow-still.png';
-          console.log(fire);
-          console.log(img.src);
-          if(img.src == fire){
-              img.src = 'assets/gif/fire.gif';
-          }
-          else if(img.src == snow) {
-              img.src = 'assets/gif/snow.gif';
-          }
-      }
-  }
+    ngOnInit(): void {
+        this.leaderboardService
+            .getLeaderBoard()
+            .subscribe((users) => {
+                this.users = users;
+                console.log(this.users);
 
-  turnToStatic(e: Event) : void {
-      const parent = e.target as HTMLElement;
-      if(parent.children[2].children[0]){
-          const img = parent.children[2].children[0].children[0] as HTMLImageElement;
-          const fire: string = window.location.origin + '/assets/gif/fire.gif';
-          const snow: string = window.location.origin + '/assets/gif/snow.gif';
-          if(img.src == fire){
-              img.src = 'assets/gif/fire-still.png';
-          }
-          else if(img.src == snow) {
-              img.src = 'assets/gif/snow-still.png';
-          }
-      }
-  }
+                // this.users = users.sort((a, b) => {
+                //     if (a.token_value < b.token_value) {
+                //         return 1;
+                //     }
+                //     if (a.token_value > b.token_value) {
+                //         return -1;
+                //     }
+                //     return 0;
+                // });
+
+                // for (let i = 0; i < 3; i++) {
+                //     this.topThree.push(this.users.shift());
+                // }
+            });
+
+    }
+
+    ngAfterViewInit(): void {
+        this.leaderBoardData = new MatTableDataSource(this.leaderBoard);
+        console.log(this.leaderBoardData);
+    }
+    turnToGif(e: Event): void {
+        const parent = e.target as HTMLElement;
+        if (parent.children[2].children[0]) {
+            const img = parent.children[2].children[0].children[0] as HTMLImageElement;
+            const fire: string = window.location.origin + '/assets/gif/fire-still.png';
+            const snow: string = window.location.origin + '/assets/gif/snow-still.png';
+            if (img.src == fire) {
+                img.src = 'assets/gif/fire.gif';
+            } else if (img.src == snow) {
+                img.src = 'assets/gif/snow.gif';
+            }
+        }
+    }
+
+    turnToStatic(e: Event): void {
+        const parent = e.target as HTMLElement;
+        if (parent.children[2].children[0]) {
+            const img = parent.children[2].children[0].children[0] as HTMLImageElement;
+            const fire: string = window.location.origin + '/assets/gif/fire.gif';
+            const snow: string = window.location.origin + '/assets/gif/snow.gif';
+            if (img.src == fire) {
+                img.src = 'assets/gif/fire-still.png';
+            } else if (img.src == snow) {
+                img.src = 'assets/gif/snow-still.png';
+            }
+        }
+    }
 
 }
