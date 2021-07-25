885e09c9b6094eb4a8260055c32c5b47b231a2ed
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-07-24 22:06:20 -0700

    Added tests for course event create edit

--- src/app/course/_test/course-event-create-edit/course-event-create-edit.component.spec.ts
+++ src/app/course/_test/course-event-create-edit/course-event-create-edit.component.spec.ts
@@ -4,13 +4,12 @@ import {CourseEventCreateEditComponent} from '../../course-event-create/course-e
 import {TestModule} from '@test/test.module';
 import {CourseEventServiceMock} from "@app/problems/_test/course-event.service.mock";
 import {CourseEventService} from "@app/course/_services/course-event.service";
-import {RouterModule} from "@angular/router";
+import {ActivatedRoute, convertToParamMap, RouterModule} from "@angular/router";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {NgxMatDatetimePickerModule} from "@angular-material-components/datetime-picker";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
-
 describe('CourseEventCreateComponent without EventId', () => {
     let component: CourseEventCreateEditComponent;
     let fixture: ComponentFixture<CourseEventCreateEditComponent>;
@@ -27,6 +26,16 @@ describe('CourseEventCreateComponent without EventId', () => {
             declarations: [CourseEventCreateEditComponent],
             providers: [
                 {provide: CourseEventService, useClass: CourseEventServiceMock},
+                {
+                    provide: ActivatedRoute, useValue: {
+                        snapshot: {
+                            paramMap: convertToParamMap({
+                                eventId: 1,
+                                courseId: 1
+                            })
+                        }
+                    }
+                }
             ]
         }).compileComponents();
     });
@@ -39,5 +48,9 @@ describe('CourseEventCreateComponent without EventId', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('form data should be filled with data', () => {
+        expect(component.formData).toBeTruthy();
     });
 });
