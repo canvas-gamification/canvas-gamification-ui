b09b14545611d2231b5e0ac90b0d42ac11ceff1d
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-03 14:35:03 -0700

    PR Fixes

--- src/app/_models/action.ts
+++ src/app/_models/action.ts
@@ -1,13 +1,48 @@
 import {SafeHtml} from "@angular/platform-browser";
 
+enum ActionType {
+    QUESTION = 'Question',
+    USER = 'User',
+    SUBMISSION = 'Submission',
+    COURSE = 'Course',
+    EVENT = 'Event',
+    COURSE_REGISTRATION = 'Course Registration'
+}
+
+enum ActionVerb {
+    CREATED = 'Created',
+    COMPLETED = 'Completed',
+    OPENED = 'Opened',
+    DELETED = 'Deleted',
+    DELIVERED = 'Delivered',
+    READ = 'Read',
+    SOLVED = 'Solved',
+    SUBMITTED = 'Submitted',
+    SENT = 'Sent',
+    STARTED = 'Started',
+    USED = 'Used',
+    REGISTERED = 'Registered',
+    EDITED = 'Edited',
+    UNREAD = 'Unread',
+    SKIPPED = 'Skipped',
+    LOGGED_IN = 'Logged In',
+    LOGGED_OUT = 'Logged Out',
+    EVALUATED = 'Evaluated'
+}
+
+enum ActionStatus {
+    COMPLETE = 'Complete',
+    PENDING = 'Pending'
+}
+
 export interface Action {
     id: number;
     description: string;
     safeDescription: SafeHtml;
     token_change: number;
-    status: string;
+    status: ActionStatus;
     time_created: Date;
     time_modified: Date;
-    object_type: string;
-    verb: string;
+    object_type: ActionType;
+    verb: ActionVerb;
 }
