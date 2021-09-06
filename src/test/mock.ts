3daf58651c88f8702af611afaa4fe01583a7ec98
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-06 09:16:38 -0700

    User Actions Service Testing

---
+++ src/test/mock.ts
@@ -0,0 +1,27 @@
+import {Action} from "@app/_models";
+
+export const MOCK_USER_ACTION_1: Action = {
+    description: "",
+    id: 0,
+    object_type: undefined,
+    safeDescription: undefined,
+    status: undefined,
+    time_created: undefined,
+    time_modified: undefined,
+    token_change: 0,
+    verb: undefined
+};
+
+export const MOCK_USER_ACTION_2: Action = {
+    description: "",
+    id: 1,
+    object_type: undefined,
+    safeDescription: undefined,
+    status: undefined,
+    time_created: undefined,
+    time_modified: undefined,
+    token_change: 0,
+    verb: undefined
+};
+
+export const MOCK_USER_ACTIONS: Action[] = [MOCK_USER_ACTION_1, MOCK_USER_ACTION_2];
