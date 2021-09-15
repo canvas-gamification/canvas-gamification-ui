777b89820a20087984641ff67152d0386662742d
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-15 09:13:46 -0700

    e2e config update

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome'
   },
-  directConnect: true,
+  directConnect: false,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
