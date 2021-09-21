10fc33ac9a5f659714f838b47eb16bbe2f25c5a7
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2021-09-20 18:35:01 -0700

    User Actions UI (#249)
    * Update Table for User Actions
    
    - Adds support for the new format of actions implemented on the backend.
    
    * Fix Pagination in Table
    
    * PR Fixes
    
    * User Actions Service Testing
    
    * Add User Actions Tests
    
    - Also remove unused code from the component.

--- src/app/components/homepage/user-actions/user-actions.component.spec.ts
+++ src/app/components/homepage/user-actions/user-actions.component.spec.ts
@@ -2,6 +2,11 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserActionsComponent} from './user-actions.component';
 import {TestModule} from '../../../../test/test.module';
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {UserActionsService} from "@app/_services/api/user-actions.service";
+import {UserActionsServiceMock} from "@test/user-actions.service.mock";
+import {MOCK_USER_ACTIONS} from "@test/mock";
 
 describe('UserActionsComponent', () => {
     let component: UserActionsComponent;
@@ -9,7 +14,11 @@ describe('UserActionsComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TestModule]
+            imports: [TestModule, MatPaginatorModule, MatTableModule],
+            declarations: [UserActionsComponent],
+            providers: [
+                {provide: UserActionsService, useClass: UserActionsServiceMock}
+            ]
         }).compileComponents();
     });
 
@@ -21,5 +30,20 @@ describe('UserActionsComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set user actions', () => {
+        expect(component.userActions).toEqual(MOCK_USER_ACTIONS);
+    });
+
+    it('should sort actions', () => {
+        component.sortData({active: 'ascending', direction: 'asc'});
+        expect(component.ordering).toEqual('ascending');
+
+        component.sortData({active: 'ascending', direction: 'desc'});
+        expect(component.ordering).toEqual('-ascending');
+
+        component.sortData({active: 'ascending', direction: ''});
+        expect(component.ordering).toEqual('');
     });
 });
