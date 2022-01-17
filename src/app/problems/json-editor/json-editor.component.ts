96b5716bea6c8b2e1456b534e5cec90b484d2970
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-01-17 14:56:29 -0800

    Fix Linting (#322)
    * Initial Type Changes
    
    * Suppress Warning

--- src/app/problems/json-editor/json-editor.component.ts
+++ src/app/problems/json-editor/json-editor.component.ts
@@ -13,7 +13,7 @@ export class JsonEditorComponent implements OnInit {
     // TODO - Determine the Proper Typing.
     editor: any;
     @Input() name: string;
-    @Input() value: any;
+    @Input() value: JSON | JSON[];
     @Output() readonly valueChange = new EventEmitter<JSON>();
 
     constructor(private schemaService: SchemaService) {
