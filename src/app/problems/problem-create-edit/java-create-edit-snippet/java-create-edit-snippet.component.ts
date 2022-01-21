58c5d992e63522318acde0420e54ee1de2bdfacb
MtheDV <mathewdevin03@gmail.com>
2022-01-21 02:06:36 -0800

    Refactor problem create/edit pages
     - Remove create and edit components and merge them as one component
     - Update each type of problem with taiga
     - Use new JSON editor for variables/java/...
     - Update forms
     - Update tests

---
+++ src/app/problems/problem-create-edit/java-create-edit-snippet/java-create-edit-snippet.component.ts
@@ -0,0 +1,70 @@
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {AbstractControl, FormGroup} from '@angular/forms';
+import {QuestionService} from '@app/problems/_services/question.service';
+import {JavaForm} from "@app/problems/_forms/java.form";
+import {Router} from "@angular/router";
+import {TuiNotification, TuiNotificationsService} from "@taiga-ui/core";
+
+@Component({
+    selector: 'app-java-create-snippet',
+    templateUrl: './java-create-edit-snippet.component.html',
+    styleUrls: ['./java-create-edit-snippet.component.scss'],
+})
+export class JavaCreateEditSnippetComponent implements OnInit {
+    @Input() questionDetails;
+    formGroup: FormGroup;
+
+    constructor(private questionService: QuestionService,
+                private router: Router,
+                @Inject(TuiNotificationsService) private readonly notificationsService: TuiNotificationsService) {
+    }
+
+    /**
+     * Method to get the form controls.
+     */
+    get form(): { [p: string]: AbstractControl } {
+        return this.formGroup.controls;
+    }
+
+    ngOnInit(): void {
+        if (this.questionDetails) {
+            this.formGroup = JavaForm.createFormWithData(this.questionDetails);
+        } else {
+            this.formGroup = JavaForm.createForm();
+        }
+    }
+
+    /**
+     * Form submission.
+     */
+    onSubmit(): void {
+        const submissionRequest = this.formGroup.getRawValue();
+        if (this.questionDetails) {
+            this.questionService.putJavaQuestion(submissionRequest, this.questionDetails.id)
+                .subscribe(() => {
+                    this.notificationsService
+                        .show('The Question has been Updated Successfully.', {
+                            status: TuiNotification.Success
+                        }).subscribe();
+                    this.refreshPage();
+                });
+        } else {
+            this.questionService.postJavaQuestion(submissionRequest)
+                .subscribe(() => {
+                    this.notificationsService
+                        .show('The Question has been Created Successfully.', {
+                            status: TuiNotification.Success
+                        }).subscribe();
+                    this.refreshPage();
+                });
+        }
+    }
+
+    refreshPage(): void {
+        const currentUrl = this.router.url;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate([currentUrl]).then(() => {
+            window.scroll(0, 0);
+        });
+    }
+}
--- src/app/problems/problems.module.ts
+++ src/app/problems/problems.module.ts
@@ -1,10 +1,9 @@
-import {ProblemCreateComponent} from "@app/problems/problem-create/problem-create.component";
+import {ProblemCreateEditComponent} from "@app/problems/problem-create-edit/problem-create-edit.component";
 import {HIGHLIGHT_OPTIONS, HighlightModule} from "ngx-highlightjs";
 import {
-    JavaCreateSnippetComponent
-} from "@app/problems/problem-create/java-create-snippet/java-create-snippet.component";
+    JavaCreateEditSnippetComponent
+} from "@app/problems/problem-create-edit/java-create-edit-snippet/java-create-edit-snippet.component";
 import {McqViewSnippetComponent} from "@app/problems/problem-view/mcq-view-snippet/mcq-view-snippet.component";
-import {JavaEditSnippetComponent} from "@app/problems/problem-edit/java-edit-snippet/java-edit-snippet.component";
 import {CommonModule} from "@angular/common";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {
@@ -13,37 +12,33 @@ import {
 import {NgModule} from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SubmissionSnippetComponent} from "@app/problems/problem-view/submission-snippet/submission-snippet.component";
-import {McqCreateSnippetComponent} from "@app/problems/problem-create/mcq-create-snippet/mcq-create-snippet.component";
+import {
+    McqCreateEditSnippetComponent
+} from "@app/problems/problem-create-edit/mcq-create-edit-snippet/mcq-create-edit-snippet.component";
 import {MatSortModule} from "@angular/material/sort";
 import {MatTableModule} from "@angular/material/table";
 import {JsonEditorComponent} from "@app/problems/json-editor/json-editor.component";
-import {ProblemEditComponent} from "@app/problems/problem-edit/problem-edit.component";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {AceComponent} from "@app/problems/ace/ace.component";
 import {ProblemViewComponent} from "@app/problems/problem-view/problem-view.component";
-import {McqEditSnippetComponent} from "@app/problems/problem-edit/mcq-edit-snippet/mcq-edit-snippet.component";
 import {CKEditorModule} from "@ckeditor/ckeditor5-angular";
 import {AceEditorModule} from "ng2-ace-editor";
 import {CkEditorComponent} from "@app/problems/ck-editor/ck-editor.component";
-import {
-    ParsonsEditSnippetComponent
-} from "@app/problems/problem-edit/parsons-edit-snippet/parsons-edit-snippet.component";
 import {SubmissionViewComponent} from "@app/problems/submission-view/submission-view.component";
 import {ProblemSetComponent} from "@app/problems/problem-set/problem-set.component";
 import {JavaViewSnippetComponent} from "@app/problems/problem-view/java-view-snippet/java-view-snippet.component";
 import {VariableViewComponent} from "@app/problems/problem-view/variable-view/variable-view.component";
 import {
-    ParsonsCreateSnippetComponent
-} from "@app/problems/problem-create/parsons-create-snippet/parsons-create-snippet.component";
+    ParsonsCreateEditSnippetComponent
+} from "@app/problems/problem-create-edit/parsons-create-edit-snippet/parsons-create-edit-snippet.component";
 import {ProblemsRoutingModule} from "@app/problems/problems-routing.module";
 import {DragulaModule} from "ng2-dragula";
 import {DifficultyService} from "@app/problems/_services/difficulty.service";
 import {UqjService} from "@app/problems/_services/uqj.service";
 import {QuestionService} from "@app/problems/_services/question.service";
-import {SchemaService} from "@app/problems/_services/schema.service";
 import {SubmissionService} from "@app/problems/_services/submission.service";
 import {ParsonsLinesComponent} from './problem-view/parsons-lines/parsons-lines.component';
-import {TuiTableModule, TuiTablePaginationModule} from "@taiga-ui/addon-table";
+import {TuiReorderModule, TuiTableModule, TuiTablePaginationModule} from "@taiga-ui/addon-table";
 import {
     TuiButtonModule,
     TuiDataListModule,
@@ -76,38 +71,38 @@ import {
     TuiTextAreaModule
 } from "@taiga-ui/kit";
 import {TabListViewSwitcherModule} from "@app/components/tab-list-view-switcher/tab-list-view-switcher.module";
+import {VariableComponent} from './json-editor/variable/variable.component';
 import {PipesModule} from "@app/_helpers/pipes/pipes.module";
-import {TestCasesComponent} from "@app/problems/json-editor/test-cases/test-cases.component";
-import {ParsonsInputFilesComponent} from "@app/problems/json-editor/parsons-input-files/parsons-input-files.component";
-import {JavaInputFilesComponent} from "@app/problems/json-editor/java-input-files/java-input-files.component";
-import {VariableComponent} from "@app/problems/json-editor/variable/variable.component";
+import {JavaInputFilesComponent} from './json-editor/java-input-files/java-input-files.component';
+import {ParsonsInputFilesComponent} from './json-editor/parsons-input-files/parsons-input-files.component';
+import {TestCasesComponent} from './json-editor/test-cases/test-cases.component';
+import {
+    DefaultCreateEditSnippetComponent
+} from './problem-create-edit/default-create-edit-snippet/default-create-edit-snippet.component';
 
 @NgModule({
     declarations: [
         ProblemSetComponent,
         ProblemViewComponent,
-        ProblemEditComponent,
-        ProblemCreateComponent,
+        ProblemCreateEditComponent,
         AceComponent,
         McqViewSnippetComponent,
         JavaViewSnippetComponent,
         ParsonsViewSnippetComponent,
-        ParsonsEditSnippetComponent,
-        McqEditSnippetComponent,
-        JavaEditSnippetComponent,
-        McqCreateSnippetComponent,
-        JavaCreateSnippetComponent,
-        ParsonsCreateSnippetComponent,
+        McqCreateEditSnippetComponent,
+        JavaCreateEditSnippetComponent,
+        ParsonsCreateEditSnippetComponent,
         VariableViewComponent,
         JsonEditorComponent,
         SubmissionSnippetComponent,
         SubmissionViewComponent,
         CkEditorComponent,
         ParsonsLinesComponent,
-        TestCasesComponent,
-        ParsonsInputFilesComponent,
-        JavaInputFilesComponent,
         VariableComponent,
+        JavaInputFilesComponent,
+        ParsonsInputFilesComponent,
+        TestCasesComponent,
+        DefaultCreateEditSnippetComponent,
     ],
     imports: [
         CommonModule,
@@ -142,6 +137,7 @@ import {VariableComponent} from "@app/problems/json-editor/variable/variable.com
         TuiNotificationModule,
         TuiAccordionModule,
         TuiTabsModule,
+        TuiReorderModule,
         TuiCheckboxLabeledModule,
         TuiCheckboxBlockModule,
         TuiRadioBlockModule,
@@ -159,7 +155,6 @@ import {VariableComponent} from "@app/problems/json-editor/variable/variable.com
         DifficultyService,
         UqjService,
         QuestionService,
-        SchemaService,
         SubmissionService,
         {
             provide: HIGHLIGHT_OPTIONS,
