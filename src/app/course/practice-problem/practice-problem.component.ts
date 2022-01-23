2df80010799431e261a31318a3f3026d394ca927
Keyvan <keyvankhademi@gmail.com>
2022-01-23 14:44:41 -0800

    add practice component

---
+++ src/app/course/practice-problem/practice-problem.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-practice-problem',
+    templateUrl: './practice-problem.component.html',
+    styleUrls: ['./practice-problem.component.scss']
+})
+export class PracticeProblemComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
