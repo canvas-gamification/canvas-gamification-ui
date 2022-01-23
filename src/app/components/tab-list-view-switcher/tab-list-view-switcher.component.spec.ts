b4a03c32bfcd0967f2b06e798de8c737521012ba
Mathew de Vin <37320754+MtheDV@users.noreply.github.com>
2022-01-23 08:13:34 -0800

    Problem Sets #2 - Create tab list switcher component and service (#343)
    * Add problem set form and template helpers
    
     - Form helpers
       - Confirm password validation
       - Field exists if other exists validation
     - Pipes
       - As form control pipe
       - As form group pipe
       - Stringify tui data list pipe
     - Tests
    
    * Fix file location problems after moving form validator
    
    * Add tab-list-view-switcher component and service
    
     - Service
       - Manage the variable stored in local storage to determine list or tab
     - Component
       - Use service
       - Allow user to change which view they want
       - Create as module for re-usability throughout website
    
    Co-authored-by: Keyvan <keyvankhademi@gmail.com>

---
+++ src/app/components/tab-list-view-switcher/tab-list-view-switcher.component.spec.ts
@@ -0,0 +1,39 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {TabListViewSwitcherComponent} from './tab-list-view-switcher.component';
+import {TestModule} from "@test/test.module";
+import {TuiRadioBlockModule} from "@taiga-ui/kit";
+import {TuiSvgModule} from "@taiga-ui/core";
+
+describe('TabListViewSwitcherComponent', () => {
+    let component: TabListViewSwitcherComponent;
+    let fixture: ComponentFixture<TabListViewSwitcherComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TabListViewSwitcherComponent],
+            imports: [TestModule, TuiRadioBlockModule, TuiSvgModule]
+        })
+            .compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TabListViewSwitcherComponent);
+        component = fixture.componentInstance;
+        window.localStorage.removeItem(component['tabListViewService'].localStorageValue);
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get initiated view with default list value', () => {
+        expect(component.view).toBe('list');
+    });
+
+    it('should set view', () => {
+        component.setView('tab');
+        expect(component['tabListViewService'].getView()).toBe('tab');
+    });
+});
