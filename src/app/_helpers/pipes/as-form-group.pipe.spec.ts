6b6be82c196d5c1f7358fb76b4693400c52096d1
Mathew de Vin <37320754+MtheDV@users.noreply.github.com>
2022-01-23 08:07:19 -0800

    Problem Sets #1 - Add problem set form and template helpers (#341)
    * Add problem set form and template helpers
    
     - Form helpers
       - Confirm password validation
       - Field exists if other exists validation
     - Pipes
       - As form control pipe
       - As form group pipe
       - Stringify tui data list pipe
     - Tests
    
    * Fix file location problems after moving form validator

---
+++ src/app/_helpers/pipes/as-form-group.pipe.spec.ts
@@ -0,0 +1,13 @@
+import {AsFormGroupPipe} from './as-form-group.pipe';
+
+describe('AsFormGroupPipe', () => {
+    let pipe: AsFormGroupPipe;
+
+    beforeEach(() => {
+        pipe = new AsFormGroupPipe();
+    });
+
+    it('should create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+});
---
+++ src/app/_helpers/pipes/as-form-group.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {AbstractControl, FormGroup} from "@angular/forms";
+
+/**
+ * Given an AbstractControl object, return
+ * the value as a FormGroup object
+ */
+@Pipe({
+    name: 'asFormGroup'
+})
+export class AsFormGroupPipe implements PipeTransform {
+    transform(value: AbstractControl): FormGroup {
+        return value as FormGroup;
+    }
+}
