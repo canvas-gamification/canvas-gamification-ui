958bba6c11ce3069de815b96ab620a518b31a922
Keyvan <keyvankhademi@gmail.com>
2022-02-02 16:32:15 -0800

    Fix ckEditor not updating

--- src/app/problems/ck-editor/ck-editor.component.ts
+++ src/app/problems/ck-editor/ck-editor.component.ts
@@ -1,17 +1,22 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, ViewChild} from '@angular/core';
 import * as ClassicEditor from "@canvas-gamification/ckeditor5";
-import {ChangeEvent} from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import {ChangeEvent, CKEditorComponent} from '@ckeditor/ckeditor5-angular/ckeditor.component';
 
 @Component({
     selector: 'app-ck-editor',
     templateUrl: './ck-editor.component.html',
     styleUrls: ['./ck-editor.component.scss']
 })
-export class CkEditorComponent {
+export class CkEditorComponent implements OnChanges {
     @Input() readOnly = false;
     @Input() value: string;
     @Output() readonly valueChange = new EventEmitter<string>();
     public ckEditor = ClassicEditor;
+    @ViewChild('editor') editorComponent: CKEditorComponent;
+
+    ngOnChanges(): void {
+        this.editorComponent.editorInstance.setData(this.value);
+    }
 
     /**
      * Emitter for when the text in the editor is changed.
