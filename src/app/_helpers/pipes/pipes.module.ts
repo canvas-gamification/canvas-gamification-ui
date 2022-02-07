941859daeaa91c15a6525ac991b8fab68df2975d
MtheDV <mathewdevin03@gmail.com>
2022-02-07 14:54:58 -0800

    Add tui status pipe to remove duplicate functions
     - Add pipe
     - Remove functions in components
     - Use pipe in templates

--- src/app/_helpers/pipes/pipes.module.ts
+++ src/app/_helpers/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AsFormGroupPipe} from "@app/_helpers/pipes/as-form-group.pipe";
 import {AsFormControlPipe} from './as-form-control.pipe';
-import { StringifyTuiDataListPipe } from './stringify-tui-data-list.pipe';
-import { GetColorFromStringPipe } from './get-color-from-string.pipe';
-import { UserHasCourseViewPermissionsPipe } from './user-has-course-view-permissions.pipe';
+import {StringifyTuiDataListPipe} from './stringify-tui-data-list.pipe';
+import {GetColorFromStringPipe} from './get-color-from-string.pipe';
+import {UserHasCourseViewPermissionsPipe} from './user-has-course-view-permissions.pipe';
+import {TuiStatusPipe} from './tui-status.pipe';
 
 
 @NgModule({
@@ -14,13 +15,15 @@ import { UserHasCourseViewPermissionsPipe } from './user-has-course-view-permiss
         StringifyTuiDataListPipe,
         GetColorFromStringPipe,
         UserHasCourseViewPermissionsPipe,
+        TuiStatusPipe,
     ],
     exports: [
         AsFormGroupPipe,
         AsFormControlPipe,
         StringifyTuiDataListPipe,
         UserHasCourseViewPermissionsPipe,
-        GetColorFromStringPipe
+        GetColorFromStringPipe,
+        TuiStatusPipe
     ],
     imports: [
         CommonModule
