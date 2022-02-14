f6486d302b4d48ac86a008ccdcf6ee882343c7f9
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-02-14 11:16:19 -0800

    Fix Submissions not Reloading Upon Submission

--- src/app/problems/problem-view/parsons-view-snippet/parsons-view-snippet.component.ts
+++ src/app/problems/problem-view/parsons-view-snippet/parsons-view-snippet.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
 import {ParsonsFile, UQJ} from '@app/_models';
 import {SubmissionService} from '@app/problems/_services/submission.service';
 import {TuiNotification, TuiNotificationsService} from "@taiga-ui/core";
@@ -10,7 +10,7 @@ import {TuiNotification, TuiNotificationsService} from "@taiga-ui/core";
 })
 export class ParsonsViewSnippetComponent implements OnInit {
     @Input() uqj: UQJ;
-
+    @Output() readonly successfulSubmissionEvent = new EventEmitter<boolean>();
     files: (ParsonsFile & { solution: string })[]
 
     constructor(private submissionService: SubmissionService,
@@ -40,6 +40,7 @@ export class ParsonsViewSnippetComponent implements OnInit {
                 .show('The Question has been Submitted Successfully.', {
                     status: TuiNotification.Success
                 }).subscribe();
+            this.successfulSubmissionEvent.emit(true);
         });
     }
 }
