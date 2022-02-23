fecea4699a2f69d79c467a2613623db02b64c308
MtheDV <mathewdevin03@gmail.com>
2022-02-22 23:49:11 -0800

    Add global footer to app
     - Add footer module and component
     - Add to app component template
     - Determine when to show the footer (Since the landing page has a special footer)
     - Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router, RouterEvent} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,13 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     title = 'canvas-gamification-ui';
+    hideFooterForLanding = true;
+
+    constructor(private router: Router) {
+        this.router.events.subscribe((event) => {
+            if (event instanceof RouterEvent) {
+                this.hideFooterForLanding = event.url === '/';
+            }
+        });
+    }
 }
