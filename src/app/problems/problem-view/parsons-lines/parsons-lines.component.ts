421fc9005da1cee25106b582ce9abcb85d8702fe
Mathew de Vin <mathewdevin03@gmail.com>
2022-03-16 18:06:29 -0700

    Update parsons lines
     - Make more user-friendly
       - Instead of nesting by manually adding indents allow nested objects by having multiple dragula containers
       - Visually nicer with styling updates
       - Hover states
     - Add cases for certain brace cases in Java
       - if(), else(), while(), for() without braces
       - These, alongside curly braces, allow objects to nest. Otherwise, if they don't meet conditions you can't nest.

--- src/app/problems/problem-view/parsons-lines/parsons-lines.component.ts
+++ src/app/problems/problem-view/parsons-lines/parsons-lines.component.ts
@@ -1,78 +1,109 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ParsonsFile} from "@app/_models";
-import * as indentString from "indent-string";
-import {DragulaService} from "ng2-dragula";
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {ParsonsFile} from '@app/_models';
+import {DragulaService} from 'ng2-dragula';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-parsons-lines',
     templateUrl: './parsons-lines.component.html',
     styleUrls: ['./parsons-lines.component.scss']
 })
-export class ParsonsLinesComponent implements OnInit {
+export class ParsonsLinesComponent implements OnInit, OnDestroy {
     @Input() file: ParsonsFile;
     @Output() readonly code = new EventEmitter<string>();
 
-    leftContainer: ContainerObject[];
-    rightContainer: ContainerObject[];
+    leftContainer: ContainerObject[] = [];
+    rightContainer: ContainerObject[] = [];
 
-    constructor(private dragulaService: DragulaService,) {
+    subscriptions: Subscription = new Subscription();
+
+    constructor(private dragulaService: DragulaService) {
     }
 
     ngOnInit(): void {
-        this.leftContainer = [];
-        this.rightContainer = [];
         for (const line of this.file.lines) {
             this.leftContainer.push(new ContainerObject(line));
         }
-        this.removeLeftContainerIndents();
-        this.dragulaService.createGroup(this.file.name, {});
-
-        this.dragulaService.dragend().subscribe(() => {
-            this.determineIndents();
-            this.removeLeftContainerIndents();
-            this.calculateSourceCode();
+        this.dragulaService.createGroup(this.file.name, {
+            revertOnSpill: true
         });
+
+        this.subscriptions.add(
+            this.dragulaService.dragend().subscribe(() => {
+                this.calculateSourceCode();
+            })
+        );
+        // These two subscriptions are used as a workaround to stop nested dragula arrays
+        // from giving an error with "Node.insertBefore: The new child is an ancestor of the parent"
+        this.subscriptions.add(
+            this.dragulaService.over().subscribe(({el}) => {
+                if (el.classList.contains('gu-transit')) {
+                    el.querySelectorAll('.container').forEach(child => {
+                        child.classList.add('remove-container-events');
+                    });
+                }
+            })
+        );
+        this.subscriptions.add(
+            this.dragulaService.dragend().subscribe(({el}) => {
+                el.querySelectorAll('.container').forEach(child => {
+                    child.classList.remove('remove-container-events');
+                });
+            })
+        );
     }
 
-    /**
-     * Determine indents for the parsons lines.
-     */
-    determineIndents(): void {
-        let count = 0;
-        this.rightContainer.forEach(line => {
-            const tempLine = line.value.trim();
-            if (tempLine.charAt(tempLine.length - 1) === '}') {
-                count = Math.max(count - 1, 0);
-            }
-            line.value = indentString(tempLine, count, {indent: '    '});
-            if (tempLine.charAt(tempLine.length - 1) === '{') {
-                count++;
-            }
-        });
-    }
-
-    /**
-     * Remove indents from lines in the left container.
-     */
-    removeLeftContainerIndents(): void {
-        this.leftContainer.forEach(line => {
-            line.value = line.value.trim();
-        });
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
     }
 
     /**
      * Calculate the source code for question submission.
      */
     calculateSourceCode(): void {
-        let answer = '';
-        this.rightContainer.forEach(line => {
-            answer += line.value + '\n';
-        });
+        const answer = this.rightContainer.reduce((prev, curr) => {
+            return prev + curr.render('    ');
+        }, '');
         this.code.emit(answer);
     }
 }
 
 class ContainerObject {
+    canNestObjects = false;
+    nestedObjects: Array<ContainerObject> = [];
+
     constructor(public value: string) {
+        this.canNestObjects = this.determineIfNestObjects(value);
+    }
+
+    determineIfNestObjects(value: string) {
+        let matchExpression = false;
+        const elseIfMatch = /(else\s)?if\s*\((.*?)\)\s*{?/;
+        const elseMatch = /else\s*{?/;
+        const whileMatch = /while\s*\((.*?)\)\s*{?/;
+        const forMatch = /for\s*\((.*?)\)\s*{?/;
+        const curlyBraceMatch = /(.*?){/;
+        [elseIfMatch, elseMatch, whileMatch, forMatch, curlyBraceMatch].forEach(regExp => {
+            const match = value.match(regExp);
+            if (match && !matchExpression) matchExpression = match[0] === value;
+        });
+        return matchExpression;
+    }
+
+    /**
+     * Render the object to a string with nested objects as indented strings.
+     * Each object, including nested, is created with a \n separator
+     * @param indent the type of indent
+     * @param indentFactor recursive value used to determine indents for nested values.
+     */
+    render(indent: string, indentFactor = 0): string {
+        let indents = '';
+        for (let i = 0; i < indentFactor; ++i) {
+            indents += indent;
+        }
+        const initialVal = indents + this.value + '\n';
+        return this.nestedObjects.reduce((prev, curr) => {
+            return prev + curr.render(indent, indentFactor + 1);
+        }, initialVal);
     }
 }
