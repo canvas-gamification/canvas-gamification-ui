a03967164356c95f45d9db0a717d0503ac06f4e6
Mathew de Vin <37320754+MtheDV@users.noreply.github.com>
2022-04-26 14:42:09 -0700

    Finish admin page (#482)
    * Finish styling admin page
    
     - Use more up-to-date styling
     - Finish dialogs for category stats
    
    * Fix sidebar height

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {QuestionCountComponent} from './question-count/question-count.component';
 import {CategoryStatsComponent} from './category-stats/category-stats.component';
-import {TuiTableModule} from "@taiga-ui/addon-table";
+import {TuiTableModule} from '@taiga-ui/addon-table';
 import {
     TuiDataListWrapperModule,
     TuiHighlightModule,
@@ -10,11 +10,11 @@ import {
     TuiIslandModule,
     TuiSelectModule,
     TuiTabsModule
-} from "@taiga-ui/kit";
-import {FormsModule} from "@angular/forms";
-import {TuiLoaderModule, TuiScrollbarModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
-import {TuiRingChartModule} from "@taiga-ui/addon-charts";
-import {TuiFilterPipeModule} from "@taiga-ui/cdk";
+} from '@taiga-ui/kit';
+import {FormsModule} from '@angular/forms';
+import {TuiButtonModule, TuiLoaderModule, TuiScrollbarModule, TuiTextfieldControllerModule} from '@taiga-ui/core';
+import {TuiRingChartModule} from '@taiga-ui/addon-charts';
+import {TuiFilterPipeModule} from '@taiga-ui/cdk';
 import {CoursesComponent} from './courses/courses.component';
 
 
@@ -28,6 +28,7 @@ import {CoursesComponent} from './courses/courses.component';
     imports: [
         CommonModule,
         FormsModule,
+        TuiButtonModule,
         TuiDataListWrapperModule,
         TuiFilterPipeModule,
         TuiHighlightModule,
