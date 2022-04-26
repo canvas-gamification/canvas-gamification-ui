272a3e53c62ebd2ef40760b2e59f9daba4a1d4cc
Mathew de Vin <mathewdevin03@gmail.com>
2022-04-25 17:15:22 -0700

    Remove submission view route

--- src/app/problems/problems-routing.module.ts
+++ src/app/problems/problems-routing.module.ts
@@ -4,14 +4,12 @@ import {ProblemSetComponent} from "@app/problems/problem-set/problem-set.compone
 import {AuthGuard} from "@app/_helpers/auth.guard";
 import {ProblemViewComponent} from "@app/problems/problem-view/problem-view.component";
 import {ProblemCreateEditComponent} from "@app/problems/problem-create-edit/problem-create-edit.component";
-import {SubmissionViewComponent} from "@app/problems/submission-view/submission-view.component";
 
 const routes: Routes = [
     {path: '', component: ProblemSetComponent, canActivate: [AuthGuard]},
     {path: ':id', component: ProblemViewComponent, canActivate: [AuthGuard]},
     {path: ':id/edit', component: ProblemCreateEditComponent, canActivate: [AuthGuard]},
     {path: 'create/:type', component: ProblemCreateEditComponent, canActivate: [AuthGuard]},
-    {path: 'submission/:id', component: SubmissionViewComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
