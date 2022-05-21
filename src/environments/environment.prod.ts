e7d4b94c2ded3c7b8b471ac7f3ed4760ba025aa7
Keyvan <keyvankhademi@gmail.com>
2022-05-21 14:16:04 -0700

    change api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://s202.ok.ubc.ca',
+    apiBaseUrl: 'https://gamification.ok.ubc.ca',
     siteKey: '6LeEo8MUAAAAAOzXHyRF-Mq7O6MNvqUwIXsQbC_U'
 };
