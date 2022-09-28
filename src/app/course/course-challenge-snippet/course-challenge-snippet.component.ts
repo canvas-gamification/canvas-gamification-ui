14d9f2f5091687f0b6a34545f80acb1063476675
yuqi88 <snow34545@gmail.com>
2022-09-28 13:44:49 -0700

    remove sorting

--- src/app/course/course-challenge-snippet/course-challenge-snippet.component.ts
+++ src/app/course/course-challenge-snippet/course-challenge-snippet.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core'
-import {Course, CourseEvent, EventType, User} from "@app/_models"
+import {Course, CourseEvent, User} from "@app/_models"
 import {AuthenticationService} from "@app/_services/api/authentication"
-import {CourseEventService} from "@app/course/_services/course-event.service"
+
 
 @Component({
     selector: 'app-course-challenge-snippet',
@@ -14,7 +14,6 @@ export class CourseChallengeSnippetComponent implements OnInit{
     user: User
     courseEvents: CourseEvent[]
     currentDate: Date = new Date()
-
     upcomingEvents: CourseEvent[]
     pastEvents: CourseEvent[]
 
@@ -23,8 +22,6 @@ export class CourseChallengeSnippetComponent implements OnInit{
 
     ngOnInit(): void {
         this.authenticationService.currentUser.subscribe(user => this.user = user)
-
-        this.events.sort((e1,e2) => new Date(e1.start_date).getTime() - new Date(e2.start_date).getTime())
         this.upcomingEvents = this.events.filter(event => event.is_open || event.is_not_available_yet).filter(event => event.type == "CHALLENGE")
         this.pastEvents = this.events.filter(event => event.is_closed).filter(event => event.type == "CHALLENGE")
     }
