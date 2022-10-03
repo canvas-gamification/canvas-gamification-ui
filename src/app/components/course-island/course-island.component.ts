a2d3aeb295bb451149b22f93982e3ba1b14e4ed8
Jayati <jae186@gmail.com>
2022-10-03 11:28:59 -0700

    Changed the view settings for courses. Students can only see it if it's before the end date, and it says Open or Register depending on if they have registered. Admins see a button called view after the end date.

--- src/app/components/course-island/course-island.component.ts
+++ src/app/components/course-island/course-island.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input} from '@angular/core'
-import {Course, STATUS} from "@app/_models"
+import {Course, STATUS, User} from "@app/_models"
+import {AuthenticationService} from "@app/_services/api/authentication"
+import {CourseService} from "@app/course/_services/course.service"
+import {ActivatedRoute} from "@angular/router"
 
 @Component({
     selector: 'app-course-island',
@@ -10,5 +13,20 @@ export class CourseIslandComponent {
     @Input() course: Course
     @Input() hasViewPermission: boolean
     @Input() skeleton = false
+    user: User
     readonly STATUS = STATUS
+    currentDate = new Date()
+    endDate: Date
+
+    constructor(
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(user => this.user = user)
+    }
+
+    ngOnInit(): void {
+        this.endDate = new Date(this.course.end_date)
+    }
+
+
 }
