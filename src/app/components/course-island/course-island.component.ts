ca7ceec68aebc3de8efa3c6feea71a917178a0ec
Jayati <jae186@gmail.com>
2022-10-03 12:42:38 -0700

    made linting changes

--- src/app/components/course-island/course-island.component.ts
+++ src/app/components/course-island/course-island.component.ts
@@ -1,15 +1,14 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
 import {Course, STATUS, User} from "@app/_models"
 import {AuthenticationService} from "@app/_services/api/authentication"
-import {CourseService} from "@app/course/_services/course.service"
-import {ActivatedRoute} from "@angular/router"
+
 
 @Component({
     selector: 'app-course-island',
     templateUrl: './course-island.component.html',
     styleUrls: ['./course-island.component.scss']
 })
-export class CourseIslandComponent {
+export class CourseIslandComponent implements OnInit{
     @Input() course: Course
     @Input() hasViewPermission: boolean
     @Input() skeleton = false
@@ -18,9 +17,7 @@ export class CourseIslandComponent {
     currentDate = new Date()
     endDate: Date
 
-    constructor(
-        private authenticationService: AuthenticationService
-    ) {
+    constructor(private authenticationService: AuthenticationService) {
         this.authenticationService.currentUser.subscribe(user => this.user = user)
     }
 
