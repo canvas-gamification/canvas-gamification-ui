e0d92e8004356e84698f91085bc5374c0be2a83b
Jayati <jae186@gmail.com>
2022-10-04 22:28:11 -0700

    added course label to page

--- src/app/course/course-stats/course-stats.component.ts
+++ src/app/course/course-stats/course-stats.component.ts
@@ -1,15 +1,32 @@
 import {Component, OnInit} from '@angular/core'
-
+import {AuthenticationService} from '@app/_services/api/authentication'
+import {CourseService} from '@app/course/_services/course.service'
+import {Course, User} from '@app/_models'
+import {ActivatedRoute} from '@angular/router'
 @Component({
     selector: 'app-course-stats',
     templateUrl: './course-stats.component.html',
     styleUrls: ['./course-stats.component.scss']
 })
 export class CourseStatsComponent implements OnInit {
+    course: Course
+    courseId: number
+    user: User
 
-
-    ngOnInit(): void {
-        console.log()
+    constructor(
+        private authenticationService: AuthenticationService,
+        private courseService: CourseService,
+        private route: ActivatedRoute
+    ) {
+        this.courseId = this.route.snapshot.params.courseId
+        this.authenticationService.currentUser.subscribe(user => this.user = user)
     }
 
+    ngOnInit(): void {
+        this.courseService.getCourse(this.courseId).subscribe(course => {
+            this.course = course
+        })
+    }
 }
+
+
