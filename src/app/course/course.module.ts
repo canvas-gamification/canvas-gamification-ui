ebd91e8c0a03101c3725119483a3a94c3a865371
yuqi88 <snow34545@gmail.com>
2022-10-13 13:05:14 -0700

    remove unused modules

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -37,7 +37,7 @@ import {
     TuiButtonModule,
     TuiCalendarModule,
     TuiDataListModule,
-    TuiDialogModule, TuiDropdownModule,
+    TuiDialogModule,
     TuiErrorModule,
     TuiHostedDropdownModule,
     TuiLinkModule,
