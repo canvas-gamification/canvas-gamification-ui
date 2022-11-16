51f548c5db7a4ca543d8b79e5de4f881f82f222f
Jayati <48756230+jae186@users.noreply.github.com>
2022-11-15 21:22:59 -0800

    updated stats page with api calls and tests (#23)
    * created stats page, fixed routing, added stats content
    
    * fixed import for stats
    
    * path disappeared so re-added it
    
    * made changes to design, added subtables
    
    * finished with front-end formatting + placeholders
    
    * front end passes linting
    
    * cleaned up code
    
    * added tokens call and cleaned up code
    
    * added backend connections everything works except question stats
    
    * fixed lots of mistakes with api
    
    * used category service and added tests
    
    * added changes wanted
    
    * fixed testing errors
    
    * changes made
    
    Co-authored-by: Keyvan Khademi <keyvankhademi@gmail.com>
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>

--- src/app/_models/user_difficulty_stats.ts
+++ src/app/_models/user_difficulty_stats.ts
@@ -2,3 +2,36 @@ export interface UserStats {
     difficulty: string,
     avgSuccess: number
 }
+
+export interface Stats {
+    challenge_stats: {
+        challenges_completed: number
+    },
+    goal_stats: {
+        goals_completed: number
+    },
+    question_stats: {
+        mcq: {
+            questions_attempt: number,
+            questions_solved: number,
+            avgSuccess: number
+        },
+        java: {
+            questions_attempt: number,
+            questions_solved: number,
+            avgSuccess: number
+        },
+        parsons:{
+            questions_attempt: number,
+            questions_solved: number,
+            avgSuccess: number
+        }
+    },
+    category_stats: {
+        category: number,
+        difficulty: string,
+        questions_attempt: number,
+        questions_solved: number,
+        avgSuccess: number
+    }[]
+}
