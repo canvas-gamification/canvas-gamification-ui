c91978055958dd8c76d6236c818121ccf6b5c91a
Hedgemon4 <sethlinc4@gmail.com>
2022-11-15 16:40:03 -0800

    Undid change in course service

--- src/app/course/_services/course.service.ts
+++ src/app/course/_services/course.service.ts
@@ -82,6 +82,7 @@ export class CourseService {
             .pipe(catchError(this.apiService.handleError<Course[]>('Unable to load courses.', [])))
     }
 
+
     /**
      * Retrieve a specific course with it's info
      * @param courseId - Corresponds to the id of the course, NOT the course_id field
