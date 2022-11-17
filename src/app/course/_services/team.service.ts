b10aa448e21c35ee4db787adc477e78f8ceffd8b
Lydia Lin <60047943+yuqi88@users.noreply.github.com>
2022-11-16 17:21:23 -0800

    Team service (#537)
    * add team model
    
    * add team service

---
+++ src/app/course/_services/team.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core'
+import {HttpClient, HttpParams} from "@angular/common/http"
+import {ApiService} from "@app/_services/api.service"
+import {Team} from "@app/_models/team"
+import {Observable} from "rxjs"
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TeamService {
+
+    constructor(
+        private http: HttpClient,
+        private apiService: ApiService
+    ) {
+    }
+
+    getChallengeTeams(eventId: number): Observable<Team[]> {
+        const url = this.apiService.getURL('team')
+        const params = new HttpParams()
+            .set('event', String(eventId))
+        return this.http
+            .get<Team[]>(url, {params})
+    }
+}
