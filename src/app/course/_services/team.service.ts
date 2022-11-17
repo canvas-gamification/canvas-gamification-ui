9548f1b805639b8b39790a6b48090fdb91a724aa
yuqi88 <snow34545@gmail.com>
2022-11-17 14:29:24 -0800

    add getTeam to team service

--- src/app/course/_services/team.service.ts
+++ src/app/course/_services/team.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http"
 import {ApiService} from "@app/_services/api.service"
 import {Team} from "@app/_models/team"
 import {Observable} from "rxjs"
+import {User} from "@app/_models"
 
 @Injectable({
     providedIn: 'root'
@@ -21,5 +22,14 @@ export class TeamService {
             .set('event', String(eventId))
         return this.http
             .get<Team[]>(url, {params})
+    }
+
+    getTeam(eventId: number, user: User): Observable<Team> {
+        const url = this.apiService.getURL('team')
+        const params = new HttpParams()
+            .set('event', String(eventId))
+            .set("user", String(user))
+        return this.http
+            .get<Team>(url, {params})
     }
 }
