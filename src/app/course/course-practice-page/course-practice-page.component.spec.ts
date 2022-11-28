8fbff3fb24b8db59ae6c8ad8fafe0b87a0951b99
Hedgemon4 <sethlinc4@gmail.com>
2022-11-28 12:05:41 -0800

    Started updating tests

--- src/app/course/course-practice-page/course-practice-page.component.spec.ts
+++ src/app/course/course-practice-page/course-practice-page.component.spec.ts
@@ -1,6 +1,6 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import {CoursePracticePageComponent} from './course-practice.component'
+import {CoursePracticePageComponent} from './course-practice-page.component'
 import {CourseService} from "@app/course/_services/course.service"
 import {CourseServiceMock} from "@test/course.service.mock"
 import {MOCK_COURSE1} from "@app/course/_test/mock"
