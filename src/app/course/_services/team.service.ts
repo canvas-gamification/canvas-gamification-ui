0d9175776c9ce278b528aebd1f6b94347f61a399
yuqi88 <snow34545@gmail.com>
2022-12-01 13:58:43 -0800

    add catch error for team service

--- src/app/course/_services/team.service.ts
+++ src/app/course/_services/team.service.ts
@@ -3,7 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http"
 import {ApiService} from "@app/_services/api.service"
 import {Team} from "@app/_models/team"
 import {Observable} from "rxjs"
-import {User} from "@app/_models"
+import {catchError} from "rxjs/operators"
 
 @Injectable({
     providedIn: 'root'
@@ -24,12 +24,12 @@ export class TeamService {
             .get<Team[]>(url, {params})
     }
 
-    getMyTeam(eventId: number, user: User): Observable<Team> {
-        const url = this.apiService.getURL('team')
+    getMyTeam(eventId: number): Observable<Team> {
+        const url = this.apiService.getURL('team', 'my-team')
         const params = new HttpParams()
-            .set('event', String(eventId))
-            .set("user", String(user))
+            .set('event_id', String(eventId))
         return this.http
             .get<Team>(url, {params})
+            .pipe(catchError(this.apiService.handleError<Team>(`Error occurred while fetching My Team.`)))
     }
 }
