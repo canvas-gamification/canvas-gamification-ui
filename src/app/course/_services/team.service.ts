94ad10de81848292132ea38d9f9cef468d2a637e
Lydia Lin <60047943+yuqi88@users.noreply.github.com>
2022-12-06 21:50:51 -0800

    Add getMyTeam to team service (#541)
    * add getTeam to team service
    
    * change wording
    
    * add to team interface
    
    * add catch error for team service
    
    * add catch error for team service

--- src/app/course/_services/team.service.ts
+++ src/app/course/_services/team.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http"
 import {ApiService} from "@app/_services/api.service"
 import {Team} from "@app/_models/team"
 import {Observable} from "rxjs"
+import {catchError} from "rxjs/operators"
 
 @Injectable({
     providedIn: 'root'
@@ -21,5 +22,15 @@ export class TeamService {
             .set('event', String(eventId))
         return this.http
             .get<Team[]>(url, {params})
+            .pipe(catchError(this.apiService.handleError<Team[]>(`Error occurred while fetching all teams of this challenge.`)))
+    }
+
+    getMyTeam(eventId: number): Observable<Team> {
+        const url = this.apiService.getURL('team', 'my-team')
+        const params = new HttpParams()
+            .set('event_id', String(eventId))
+        return this.http
+            .get<Team>(url, {params})
+            .pipe(catchError(this.apiService.handleError<Team>(`Error occurred while fetching My Team.`)))
     }
 }
