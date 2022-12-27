1274c693531dc44eb6b64e2d32b192d4d4d4c7bf
Keyvan <keyvankhademi@gmail.com>
2022-12-27 12:01:24 -0800

    fix unauthenticated page views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core'
 import {NavigationEnd, Router, RouterEvent} from "@angular/router"
 import {PageViewService} from "@app/_services/api/page_view/page-view.service"
+import {AuthenticationService} from "@app/_services/api/authentication"
 
 @Component({
     selector: 'app-root',
@@ -11,12 +12,19 @@ export class AppComponent {
     title = 'canvas-gamification-ui'
     hideFooterForLanding = true
 
-    constructor(private router: Router, private pageViewService: PageViewService) {
+    constructor(
+        private router: Router,
+        private pageViewService: PageViewService,
+        private authenticationService: AuthenticationService
+    ) {
         this.router.events.subscribe((event) => {
             if (event instanceof RouterEvent) {
                 this.hideFooterForLanding = event.url === '/'
             }
-            if (event instanceof NavigationEnd) {
+            if (
+                event instanceof NavigationEnd
+                && authenticationService.currentUserValue
+            ) {
                 this.pageViewService.pageView({
                     url: event.url
                 }).subscribe()
