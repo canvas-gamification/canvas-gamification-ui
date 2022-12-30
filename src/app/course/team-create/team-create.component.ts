13aa8ece4308fdd8d5ba076e4641483c634661c8
yuqi88 <snow34545@gmail.com>
2022-12-29 20:02:58 -0800

    create component

---
+++ src/app/course/team-create/team-create.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+    selector: 'app-team-create',
+    templateUrl: './team-create.component.html',
+    styleUrls: ['./team-create.component.scss']
+})
+export class TeamCreateComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
