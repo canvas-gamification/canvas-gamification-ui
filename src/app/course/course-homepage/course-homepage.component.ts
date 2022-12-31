6b9b7546f10bff88bf0b032204fa829283abdcca
Hedgemon4 <sethlinc4@gmail.com>
2022-12-31 11:58:40 -0700

    Wrote logging actions for button presses on course homepage

--- src/app/course/course-homepage/course-homepage.component.ts
+++ src/app/course/course-homepage/course-homepage.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core'
-import {ActionStatus, ActionVerb, User} from "@app/_models"
+import {ActionStatus, ActionType, ActionVerb, User} from "@app/_models"
 import {AuthenticationService} from "@app/_services/api/authentication"
 import {CourseService} from "@app/course/_services/course.service"
 import {ActivatedRoute} from "@angular/router"
@@ -24,20 +24,99 @@ export class CourseHomepageComponent {
         this.authenticationService.currentUser.subscribe(user => this.user = user)
     }
 
+    assignmentsAndExamsClickLog(): void {
+        this.userAction.createCustomAction({
+            description: 'User selected assignments and exams on the course homepage',
+            object_type: ActionType.BUTTON,
+            status: ActionStatus.COMPLETE,
+            verb: ActionVerb.CLICKED,
+            data: {
+                page_url: '/course/:courseId/homepage',
+                page_url_params: {
+                    courseId: this.courseId
+                },
+                redirect_url: '/course/:courseId/assignments-exams',
+                redirect_url_params: {
+                    courseId: this.courseId
+                }
+            }
+        }).subscribe()
+    }
+
     challengeClickLog(): void {
         this.userAction.createCustomAction({
             description: 'User selected challenges on the course homepage',
+            object_type: ActionType.BUTTON,
             status: ActionStatus.COMPLETE,
-            verb: ActionVerb.OPENED,
-        })
+            verb: ActionVerb.CLICKED,
+            data: {
+                page_url: '/course/:courseId/homepage',
+                page_url_params: {
+                    courseId: this.courseId
+                },
+                redirect_url: '/course/:courseId/challenge',
+                redirect_url_params: {
+                    courseId: this.courseId
+                }
+            }
+        }).subscribe()
+    }
+
+    leaderboardClickLog(): void {
+        this.userAction.createCustomAction({
+            description: 'User selected leaderboard on the course homepage',
+            object_type: ActionType.BUTTON,
+            status: ActionStatus.COMPLETE,
+            verb: ActionVerb.CLICKED,
+            data: {
+                page_url: '/course/:courseId/homepage',
+                page_url_params: {
+                    courseId: this.courseId
+                },
+                redirect_url: '/course/:courseId/leaderboard',
+                redirect_url_params: {
+                    courseId: this.courseId
+                }
+            }
+        }).subscribe()
     }
 
     practiceClickLog(): void {
         this.userAction.createCustomAction({
             description: 'User selected practice on the course homepage',
+            object_type: ActionType.BUTTON,
             status: ActionStatus.COMPLETE,
-            verb: ActionVerb.OPENED,
-        })
+            verb: ActionVerb.CLICKED,
+            data: {
+                page_url: '/course/:courseId/homepage',
+                page_url_params: {
+                    courseId: this.courseId
+                },
+                redirect_url: '/course/:courseId/practice',
+                redirect_url_params: {
+                    courseId: this.courseId
+                }
+            }
+        }).subscribe()
+    }
+
+    tokenClickLog(): void {
+        this.userAction.createCustomAction({
+            description: 'User selected tokens on the course homepage',
+            object_type: ActionType.BUTTON,
+            status: ActionStatus.COMPLETE,
+            verb: ActionVerb.CLICKED,
+            data: {
+                page_url: '/course/:courseId/homepage',
+                page_url_params: {
+                    courseId: this.courseId
+                },
+                redirect_url: '/course/:courseId/tokens',
+                redirect_url_params: {
+                    courseId: this.courseId
+                }
+            }
+        }).subscribe()
     }
 
 }
