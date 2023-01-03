e3759f2f0575bef64db78f626f3af92163a4b5ed
Keyvan <keyvankhademi@gmail.com>
2023-01-02 19:36:51 -0800

    add featured assignment button

--- src/app/_models/course_event.ts
+++ src/app/_models/course_event.ts
@@ -1,16 +1,17 @@
 export interface CourseEvent {
-    id?: number;
-    name: string;
-    type: string;
-    count_for_tokens: boolean;
-    start_date: Date;
-    end_date: Date;
-    course: number;
-    is_allowed_to_open?: boolean;
-    has_edit_permission?: boolean;
-    is_open?: boolean;
-    is_exam?: boolean;
-    total_event_grade?: number;
-    is_not_available_yet: boolean;
-    is_closed: boolean;
+    id: number
+    name: string
+    type: string
+    count_for_tokens: boolean
+    start_date: Date
+    end_date: Date
+    course: number
+    is_allowed_to_open?: boolean
+    has_edit_permission?: boolean
+    is_open?: boolean
+    is_exam?: boolean
+    total_event_grade?: number
+    is_not_available_yet: boolean
+    is_closed: boolean
+    featured: boolean
 }
