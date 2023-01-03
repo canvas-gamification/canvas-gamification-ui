2ede375fa9a12e1f1fcc84ae784e6d7c68683fd2
yuqi88 <snow34545@gmail.com>
2023-01-03 01:51:46 -0800

    add team form

--- src/app/course/_forms/team.form.ts
+++ src/app/course/_forms/team.form.ts
@@ -1,10 +1,12 @@
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms"
+import {Team} from "@app/_models/team"
 
 export interface TeamFormData {
-    event_id: number
+    event: number
     name: string
     is_private: boolean
-    who_can_join: number[]
+    who_can_join?: number[]
+    course_registrations: number[] //an array of course_reg_id(s)
 }
 
 export class TeamForm {
@@ -17,15 +19,53 @@ export class TeamForm {
         })
     }
 
-    static formatTeamFormData(formData: FormGroup, eventId: number): TeamFormData{
-        return {
-            name: formData.get('name').value,
-            is_private: formData.get('is_private').value,
-            who_can_join: formData.get('invitedMembers').value,
-            event_id: eventId,
-            // course_registrations
-            // get this user's id?
-            // how do i do that--from this.authenticationService.currentUser.subscribe(user => this.user = user) ?
-        }
+    static createTeamFormTeam(team: Team): FormGroup {
+        const builder = new FormBuilder()
+        return builder.group({
+            name: new FormControl(team.name, [Validators.required]),
+            isPrivate: new FormControl(team.is_private),
+            invitedMembers: new FormControl(team.who_can_join), //array of ids
+            members: new FormControl(team.course_registrations), //array of ids
+        })
+    }
+
+    /**
+     * Returns the formatted form data ready to be sent to the backend
+     * @param formData - the data to be formatted, a FormGroup object
+     * @param eventId - the ID of the event that the team belongs to
+     * @param myCourseRegId - the team creator's course registrations id
+     */
+    static formatTeamFormData(formData: FormGroup, eventId: number, myCourseRegId: number): TeamFormData{
+
+        //Form is edited when formData.get('members').value === true; Form creation will always have 1 courseRegs which is the team creator.
+        if(formData.get('is_private').value)
+            if(formData.get('invitedMembers').value)//(is_private & has ppl invited)
+                return {
+                    event: eventId,
+                    name: formData.get('name').value,
+                    is_private: formData.get('is_private').value,
+                    // who_can_join: TeamForm.includeMySelf(myCourseRegId, formData.get('invitedMembers').value.map(courseReg => courseReg.id)),
+                    course_registrations: formData.get('members').value? formData.get('members').value.map(courseReg => courseReg.id): [myCourseRegId],
+                }
+            else //(is_private & no other ppl invited) don't need who_can_join (once I leave the team, team is deleted)
+                return {
+                    event: eventId,
+                    name: formData.get('name').value,
+                    is_private: formData.get('is_private').value,
+                    course_registrations: [myCourseRegId],
+                }
+        else //not private
+            return {
+                event: eventId,
+                name: formData.get('name').value,
+                is_private: formData.get('is_private').value,
+                course_registrations: formData.get('members').value? formData.get('invitedMembers').value.map(courseReg => courseReg.id): [myCourseRegId],
+            }
+    }
+
+    private static includeMySelf(myCourseRegId: number, invitedCourseRegIds: number[]){
+        if (invitedCourseRegIds.includes(myCourseRegId))
+            return invitedCourseRegIds
+        return invitedCourseRegIds.push(myCourseRegId)
     }
 }
