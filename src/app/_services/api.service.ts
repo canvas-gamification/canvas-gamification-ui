0056644087f14e9588f7b6e0312a859aeb630d12
Keyvan <keyvankhademi@gmail.com>
2023-01-07 12:02:33 -0800

    fix infinite redirect bug

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -42,9 +42,8 @@ export class ApiService {
             showMessage?: boolean
         }
     ): (error: HttpErrorResponse) => Observable<T> {
-        const {redirect403 = false, redirect404 = false, redirect = [], showMessage = true} = options ? options : {}
+        const {redirect403 = false, redirect404 = false, redirect = null, showMessage = true} = options ? options : {}
         return (error): Observable<T> => {
-
             if (redirect404 && error.status === 404)
                 this.router.navigate(['/404'], {skipLocationChange: true}).then()
             else if (redirect403 && error.status === 403)
