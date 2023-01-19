633ac451cd85fed68d4941ec927d4a1ca3c2795a
yuqi88 <snow34545@gmail.com>
2023-01-19 09:51:13 -0800

    add leader board page

--- src/app/course/leader-board/leader-board.component.ts
+++ src/app/course/leader-board/leader-board.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, Input, OnChanges} from '@angular/core'
 import {LeaderboardElement} from "@app/_models"
 import {CourseService} from "@app/course/_services/course.service"
 import {ActivatedRoute} from "@angular/router"
+import {CourseEventService} from "@app/course/_services/course-event.service"
 
 @Component({
     selector: 'app-leader-board',
     templateUrl: './leader-board.component.html',
     styleUrls: ['./leader-board.component.scss']
 })
-export class LeaderBoardComponent implements OnInit {
+export class LeaderBoardComponent implements OnChanges {
+    @Input() eventId: number //null for courses & exist for events
+    @Input() courseId: number
     leaderBoard: LeaderboardElement[]
     rankTopX: number
-    courseId: number
     displayedColumns: string[] = ['rank', 'name', 'token']
 
     readonly filterOutTopX = (element: LeaderboardElement, x: number): boolean => element.rank > x
@@ -19,17 +21,23 @@ export class LeaderBoardComponent implements OnInit {
 
     constructor(
         private courseService: CourseService,
-        private route: ActivatedRoute
-    ) {
-        this.courseId = this.route.snapshot.parent.params.courseId
+        private route: ActivatedRoute,
+        private courseEventService: CourseEventService,
+    ) {}
+
+    ngOnChanges(): void {
+        this.rankTopX = 3
+        if (this.eventId){
+            this.courseEventService.getEventLeaderBoard(this.eventId).subscribe(leaderboard =>
+                this.leaderBoard = this.getRankedLeaderboard(leaderboard))
+        }else{
+            this.courseService.getCourseLeaderBoard(this.courseId).subscribe(leaderboard =>
+                this.leaderBoard = this.getRankedLeaderboard(leaderboard))
+        }
+
     }
 
-    ngOnInit(): void {
-        this.rankTopX = 3
-        this.courseService.getCourse(this.courseId).subscribe(course => {
-            this.leaderBoard = this.getRankedLeaderboard(course?.leader_board)
-        })
-    }
+
 
     /**
      * Takes an unranked leaderboard, and returns a ranked version of it
@@ -38,7 +46,12 @@ export class LeaderBoardComponent implements OnInit {
     getRankedLeaderboard(leaderBoard: LeaderboardElement[]): LeaderboardElement[] {
         const sortedLeaderboard = leaderBoard.sort((a, b) => b.token - a.token)
         return sortedLeaderboard.map((element, index) => {
-            return {
+            return element.member_names ? {
+                rank: index + 1,
+                name: element.name,
+                token: element.token,
+                member_names: element.member_names
+            } : {
                 rank: index + 1,
                 name: element.name,
                 token: element.token
@@ -55,5 +68,9 @@ export class LeaderBoardComponent implements OnInit {
         const s = ["th", "st", "nd", "rd"],
             v = n % 100
         return n + (s[(v - 20) % 10] || s[v] || s[0])
+    }
+
+    getMemberNames(memberList: string[]): string {
+        return memberList.join(", ")
     }
 }
