9fdc8e82ff58667aa5511add86636e8f3b47ca22
yuqi88 <snow34545@gmail.com>
2023-01-24 14:12:55 -0800

    add sidebar

--- src/app/course/leader-board-page/leader-board-page.component.ts
+++ src/app/course/leader-board-page/leader-board-page.component.ts
@@ -1,15 +1,31 @@
-import {Component} from '@angular/core' //OnInit
+import {Component, OnInit} from '@angular/core'
+import {ActivatedRoute} from "@angular/router"
+import {CourseService} from "@app/course/_services/course.service"
+import {Course} from "@app/_models"
 
 @Component({
     selector: 'app-leader-board-page',
     templateUrl: './leader-board-page.component.html',
     styleUrls: ['./leader-board-page.component.scss']
 })
-export class LeaderBoardPageComponent  { //implements OnInit
+export class LeaderBoardPageComponent implements OnInit {
+    courseId: number
+    course: Course
+    events: Event
+    options = ['a', 'b', 'c']
 
-    // constructor() { }
-    //
-    // ngOnInit(): void {
-    // }
+    constructor(
+        // private courseEventService: CourseEventService,
+        private courseService: CourseService,
+        private route: ActivatedRoute
+    ) {
+        this.courseId = this.route.snapshot.parent.params.courseId
+    }
+
+    ngOnInit(): void {
+        this.courseService.getCourse(this.courseId).subscribe(course => this.course = course)
+
+    }
+
 
 }
