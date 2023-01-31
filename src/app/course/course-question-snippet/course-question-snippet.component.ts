9b1e37deda5df1affee3d07fe31a3bbfa474e5df
SamamthaH <79422004+SamamthaH@users.noreply.github.com>
2023-01-31 02:30:20 -0500

    Back Buttons Labelling Correct (#20)
    * altered button so the assignments and exams label was correct
    
    * formatted
    
    * got rid of if statement

--- src/app/course/course-question-snippet/course-question-snippet.component.ts
+++ src/app/course/course-question-snippet/course-question-snippet.component.ts
@@ -108,4 +108,11 @@ export class CourseQuestionSnippetComponent implements OnInit {
             this.init()
         })
     }
+
+    getBackLabel(): string {
+        if (this.getEventType() === 'challenge')
+            return 'List of Challenges'
+        else if (this.getEventType() === 'assignment' || this.getEventType() === 'exam')
+            return 'Assignments and Exams'
+    }
 }
