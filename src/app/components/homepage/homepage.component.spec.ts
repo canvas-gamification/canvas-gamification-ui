069cb8dc038a8a06b2f008fe88e2959e475855ec
Hedgemon4 <sethlinc4@gmail.com>
2023-03-04 14:32:10 -0800

    Fixed inactive course component test

--- src/app/components/homepage/homepage.component.spec.ts
+++ src/app/components/homepage/homepage.component.spec.ts
@@ -25,7 +25,7 @@ describe('HomepageComponent', () => {
         fixture.detectChanges()
     })
 
-    fit('should create', () => {
+    it('should create', () => {
         expect(component).toBeTruthy()
     })
 })
