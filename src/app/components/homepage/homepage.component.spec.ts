fbdd33468edb3077def58db4fec4e52b5f2ba276
Keyvan <keyvankhademi@gmail.com>
2023-03-06 12:12:54 -0800

    fix seth tests

--- src/app/components/homepage/homepage.component.spec.ts
+++ src/app/components/homepage/homepage.component.spec.ts
@@ -3,6 +3,8 @@ import {ComponentFixture, TestBed} from '@angular/core/testing'
 import {HomepageComponent} from './homepage.component'
 import {TestModule} from '@test/test.module'
 import {SidebarModule} from '@app/components/sidebar/sidebar.module'
+import {CourseService} from "@app/course/_services/course.service"
+import {CourseServiceMock} from "@test/_services/course.service.mock"
 
 describe('HomepageComponent', () => {
     let component: HomepageComponent
@@ -10,7 +12,10 @@ describe('HomepageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TestModule, SidebarModule]
+            imports: [TestModule, SidebarModule],
+            providers: [
+                {provide: CourseService, useClass: CourseServiceMock}
+            ]
         }).compileComponents()
     })
 
@@ -20,8 +25,7 @@ describe('HomepageComponent', () => {
         fixture.detectChanges()
     })
 
-    // TODO: (Seth) fix
-    xit('should create', () => {
+    it('should create', () => {
         expect(component).toBeTruthy()
     })
 })
