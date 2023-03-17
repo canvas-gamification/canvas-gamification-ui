3e1fd0c7f0e487aca23539153eb72505ee8c1b09
Hedgemon4 <sethlinc4@gmail.com>
2023-03-16 19:49:13 -0700

    Fixed test

--- src/app/course/course-create/course-create.component.ts
+++ src/app/course/course-create/course-create.component.ts
@@ -39,7 +39,7 @@ export class CourseCreateComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        if (this.route.snapshot.parent.params.courseId) {
+        if (this.route.snapshot.parent?.params.courseId) {
             this.courseId = this.route.snapshot.parent.params.courseId
             this.courseService.getCourse(this.courseId).subscribe(course => {
                 this.formGroup = CourseForm.createCourseFormWithData(course)
