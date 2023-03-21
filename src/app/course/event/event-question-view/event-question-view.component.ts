65ffed9852b3ab5bc214417e59e95fce94a8bced
yuqi88 <snow34545@gmail.com>
2023-03-20 17:28:30 -0700

    edit previous and next question functions

--- src/app/course/event/event-question-view/event-question-view.component.ts
+++ src/app/course/event/event-question-view/event-question-view.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {CourseEvent} from "@app/_models"
 import {UqjService} from "@app/problems/_services/uqj.service"
 import {ActivatedRoute} from "@angular/router"
 import {CourseEventService} from "@app/course/_services/course-event.service"
+import {Subscription} from "rxjs"
 
 @Component({
     selector: 'app-event-question-view',
     templateUrl: './event-question-view.component.html',
     styleUrls: ['./event-question-view.component.scss']
 })
-export class EventQuestionViewComponent implements OnInit {
+export class EventQuestionViewComponent implements OnInit, OnDestroy {
     eventId: number
     event: CourseEvent
     uqjs: number[]
     currentQuestionId: number
-    cursor: number
+    cursor = 0
+    subscriptions: Subscription = new Subscription()
 
     constructor(
         private uqjService: UqjService,
@@ -23,22 +25,29 @@ export class EventQuestionViewComponent implements OnInit {
     ) {
     }
 
-    ngOnInit() : void {
+    ngOnInit(): void {
         this.eventId = +this.route.snapshot.paramMap.get('eventId')
         this.courseEventService.getCourseEvent(this.eventId).subscribe(
             event => this.event = event
         )
-        // this.uqjService.getUQJs({filters: {question_event: this.eventId}}).subscribe(
-        //     uqjs => this.uqjs = uqjs.results
-        // )
-        this.uqjService.getUQJQuestionIds({question_event: this.eventId}).subscribe(
-            // uqjs => this.uqjs = uqjs
-            uqjs => {
-                this.uqjs = uqjs
-                console.log(uqjs)
-            }
-        )
-        this.cursor = 0
+
+        this.subscriptions.add(this.route.paramMap.subscribe(paramMap => {
+            this.uqjService.getUQJQuestionIds({question_event: this.eventId})
+                .subscribe( uqjs => {
+                    this.uqjs = uqjs
+                    this.cursor = this.uqjs.indexOf(+paramMap.get('id'))
+
+                    //not printing, but questions do change as desired
+                    //issues:
+                    // (1) url problem id does not change
+                    // (2) breadcrumb question name not updated
+                    console.log(+paramMap.get('id'))
+                })
+        }))
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe()
     }
 
     prevQuestion(): void {
