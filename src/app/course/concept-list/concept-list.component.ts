077122f34ff57458738ff694d4ee6ca6dcf56a96
te-sa <teresa_saller@freenet.de>
2023-03-21 11:02:33 -0700

    have top-level categories

--- src/app/course/concept-list/concept-list.component.ts
+++ src/app/course/concept-list/concept-list.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, Input, OnInit} from '@angular/core'
+import {CategoryService} from "@app/_services/api/category.service"
+import {Category, Course, NestedCategories} from "@app/_models"
+import {Difficulty} from "@app/_models/difficulty"
+import {DifficultyService} from "@app/problems/_services/difficulty.service"
 
 @Component({
     selector: 'app-concept-list',
@@ -6,11 +10,41 @@ import {Component, OnInit} from '@angular/core'
     styleUrls: ['./concept-list.component.scss']
 })
 export class ConceptListComponent implements OnInit {
+    categories: Category[]
+    nestedCategories: NestedCategories[]
+    difficulties: Difficulty[]
+    tableColumns: ['name', ...Array<string>] = ['name']
 
-    constructor() {
+    @Input() currCourse: Course
+
+    constructor(
+        private categoryService: CategoryService,
+        private difficultyService: DifficultyService
+    ) {
     }
 
     ngOnInit(): void {
+        this.categoryService.getCategories().subscribe(categories => {
+            this.categories = categories
+            this.nestedCategories = categories.reduce((previous, category) => {
+                if (category.parent) return previous
+                return [...previous, {
+                    category,
+                    children: categories.filter(
+                        nestedCategory => nestedCategory.parent === category.pk
+                    ).map(nestedCategory => {
+                        return {
+                            category: nestedCategory,
+                            children: [],
+                        }
+                    })
+                }]
+            }, [])
+        })
+        this.difficultyService.getDifficulties().subscribe(difficulties => {
+            this.difficulties = difficulties
+            this.tableColumns = ['name', ...difficulties.map(difficulty => difficulty[0])]
+        })
     }
 
 }
