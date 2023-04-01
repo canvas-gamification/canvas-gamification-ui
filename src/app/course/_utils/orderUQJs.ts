f33030b5e0d86e3bc5f02637d26799df52c46695
Lydia Lin <60047943+yuqi88@users.noreply.github.com>
2023-04-01 13:02:44 -0700

    Fix the display order of event questions (#665)
    * modify the sort for uqjs
    
    * organize files
    
    * change name

---
+++ src/app/course/_utils/orderUQJs.ts
@@ -0,0 +1,30 @@
+import {UQJ} from "@app/_models"
+
+
+export const orderUQJs = (uqjs: UQJ[]): UQJ[] =>  {
+    const numsArr: UQJ[] = []
+    const stringsArr: UQJ[] = []
+
+    for (const uqj of uqjs) {
+        if (containsNumbers(uqj.question.title))
+            numsArr.push(uqj)
+        else
+            stringsArr.push(uqj)
+    }
+
+    numsArr.sort((a, b) => extractNumberFromString(a.question.title)
+        - extractNumberFromString(b.question.title))
+
+    stringsArr.sort((a, b) =>
+        a.question.title.localeCompare(b.question.title))
+
+    return numsArr.concat(stringsArr)
+}
+
+const containsNumbers = (str): boolean => {
+    return str.match(/\d/) !== null
+}
+
+const extractNumberFromString = (str): number => {
+    return +str.match(/(\d+)/)[0]
+}
