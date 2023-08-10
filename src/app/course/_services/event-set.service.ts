0518f294342494521e6765eb3a726112a31f7498
yuqi88 <snow34545@gmail.com>
2023-08-09 21:23:46 -0700

    add the add, delete functions

--- src/app/course/_services/event-set.service.ts
+++ src/app/course/_services/event-set.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from "@angular/common/http"
 import {ApiService} from "@app/_services/api.service"
-// import {Observable} from "rxjs"
+import {Observable} from "rxjs"
+import {EventSet} from "@app/_models/event-set"
+import {catchError} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +14,30 @@ export class EventSetService {
         private http: HttpClient,
         private apiService: ApiService
     ) {
+    }
+
+    addEventSet(eventSetData: EventSetFormData): Observable<EventSet> {
+        const url = this.apiService.getURL('event-set')
+        return this.http
+            .post<EventSet>(url, eventSetData)
+            .pipe(catchError(
+                this.apiService.handleError<EventSet>('Error occurred while adding event set.')
+            ))
+    }
+
+    deleteEventSet(eventSetId: number): Observable<EventSet> {
+        const url = this.apiService.getURL('event-set', eventSetId)
+        return this.http
+            .delete<EventSet>(url)
+            .pipe(catchError(
+                this.apiService.handleError<EventSet>('Error occurred while deleting event set.')
+            ))
 
     }
+
+    // Decision Question: Who can edit the consistency challenge?
+    // updateEventSet():Observable<any> {
+    //
+    // }
 
 }
