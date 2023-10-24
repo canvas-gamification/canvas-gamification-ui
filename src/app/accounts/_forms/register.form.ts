b2068207a37658159bc4b973b0e9e759dc25dd3d
Seth Akins <sethlinc4@gmail.com>
2023-10-23 18:59:37 -0700

    Add First Name, Last Name, and Nickname to Sign-Up Form (#723)
    * Started adding name to registration form
    
    * Added first name, last name, and nickname to sign up form
    
    * Fixed issues with tests not running
    
    * Fixed issues with tests
    
    * fixed lint errors
    
    * Reverted changes
    
    * Addressed changes

--- src/app/accounts/_forms/register.form.ts
+++ src/app/accounts/_forms/register.form.ts
@@ -6,6 +6,9 @@ export class RegisterForm {
         const builder = new FormBuilder()
         return builder.group({
             email: new FormControl(null, [Validators.required, Validators.email]),
+            first_name: new FormControl(null, [Validators.required]),
+            last_name: new FormControl(null, [Validators.required]),
+            nickname: new FormControl(null, [Validators.required]),
             password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
             password2: new FormControl(null, [Validators.required, Validators.minLength(8)]),
             recaptcha_key: new FormControl(null, [Validators.required])
@@ -21,7 +24,10 @@ export class RegisterForm {
 
 export interface RegisterFormData {
     email: string;
-    password: string;
-    password2: string;
-    recaptcha_key: string;
+    first_name: string
+    last_name: string
+    nickname: string
+    password: string
+    password2: string
+    recaptcha_key: string
 }
