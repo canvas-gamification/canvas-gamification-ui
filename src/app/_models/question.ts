f157f843e9445568cc43fb9b75a916ff421e4616
Seth Akins <sethlinc4@gmail.com>
2024-02-25 18:59:01 -0800

    Fixed minor issues

--- src/app/_models/question.ts
+++ src/app/_models/question.ts
@@ -1,39 +1,40 @@
 import {CourseEvent} from '@app/_models/course_event'
 
 export interface Question {
-    id: number;
-    title: string;
-    text: string;
-    max_submission_allowed: number;
-    author: number;
-    category: number;
-    full_category_name: string;
-    category_name: string;
-    difficulty: string;
-    is_verified: boolean;
-    time_created: Date;
-    time_modified: Date;
-    token_value: number;
-    type_name: string;
-    event: number;
-    event_obj: CourseEvent;
-    is_sample: boolean;
-    parent_category_name: string;
-    course_name: string;
-    author_name: string;
-    choices: { [index: string]: string };
-    variables: [];
-    answer: string;
-    junit_template: string;
+    id: number
+    title: string
+    text: string
+    max_submission_allowed: number
+    author: number
+    category: number
+    full_category_name: string
+    category_name: string
+    difficulty: string
+    is_verified: boolean
+    time_created: Date
+    time_modified: Date
+    token_value: number
+    type_name: string
+    event: number
+    event_obj: CourseEvent
+    is_sample: boolean
+    parent_category_name: string
+    course_name: string
+    author_name: string
+    choices: { [index: string]: string }
+    variation_types: string[]
+    variables: []
+    answer: string
+    junit_template: string
     input_files: InputFiles,
-    status: string;
-    is_open: boolean;
-    is_exam: boolean;
-    is_exam_and_open: boolean;
-    visible_distractor_count: number;
-    is_checkbox: boolean;
-    is_author: boolean;
-    is_practice: boolean;
+    status: string
+    is_open: boolean
+    is_exam: boolean
+    is_exam_and_open: boolean
+    visible_distractor_count: number
+    is_checkbox: boolean
+    is_author: boolean
+    is_practice: boolean
 }
 
 export interface InputFiles extends Array<{
